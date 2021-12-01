59e492afb9818aed535a4d2f16010917942eb1a5
DALHOUM-Ahmed <59100760+DALHOUM-Ahmed@users.noreply.github.com>
2021-12-01 17:49:37 +0100

    Update analyzeToken.js

--- src/utils/tokenAnalyzer/withBSCAPI/analyzeToken.js
+++ src/utils/tokenAnalyzer/withBSCAPI/analyzeToken.js
@@ -3,7 +3,7 @@ const decide = require('./decide.js');
 
 const TokenAddress = "0xf32a8d06e1fe3391d52eb6026aac91cbc0292a40";
 
-const apikey = "ISMIHCDD1TKJ4R3T2X4S76YC8RT9KS2SAG";
+const apikey = "APIKEY";
 
 
 getTokenInfos(TokenAddress, apikey).then((infos) => decide(infos));
