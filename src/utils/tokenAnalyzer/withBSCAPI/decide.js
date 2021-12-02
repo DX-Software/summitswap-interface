4c64c08c8cc400e10b4cff93d59235a91a378fe6
DALHOUM-Ahmed <59100760+DALHOUM-Ahmed@users.noreply.github.com>
2021-12-02 12:21:33 +0100

    Added new metrics.

--- src/utils/tokenAnalyzer/withBSCAPI/decide.js
+++ src/utils/tokenAnalyzer/withBSCAPI/decide.js
@@ -7,9 +7,11 @@ function decide(infos) {
     console.log("token contract verified");
   }
 
-  scamScore += infos.revLikelihood;
-
-  scamScore += (50 * infos.non_transfersCount) / infos.bscTransactions.length;
+  scamScore += 10 * infos.revLikelihood;
+  infos.non_transfers_mean_deviation = Math.abs(
+    infos.non_transfersCount / infos.Ntxs - 0.5
+  );
+  scamScore += 150 * infos.non_transfers_mean_deviation;
   console.log("non_trans ", infos.non_transfersCount);
   scamScore += (80 * infos.NumberOfFailedTransactions) / infos.Ntxs;
   console.log("failed transactions", infos.NumberOfFailedTransactions);
@@ -18,11 +20,16 @@ function decide(infos) {
   if (scamScore > 40 && scamScore < 50 && infos.Ntxs < 1000) {
     scamScore += 10;
   }
-  scamScore += (infos.transferFailsRatio * 100);
+  scamScore += infos.transferFailsRatio * 100;
 
-  if (infos.Ntxs < 500 && infos.transferFailsRatio < 0.1 && (infos.NumberOfTransfers/infos.Ntxs)>0.2) {
-    console.log("probably a new non-scam token");
+  let newToken = false;
+  if (
+    infos.Ntxs < 500 &&
+    infos.transferFailsRatio < 0.1 &&
+    infos.NumberOfTransfers / infos.Ntxs > 0.2
+  ) {
     scamScore -= 20 * (1 - infos.transferFailsRatio);
+    newToken = true;
   }
   if (scamScore >= 50) {
     if (scamScore >= 100) {
@@ -33,6 +40,9 @@ function decide(infos) {
       );
     }
   } else {
+    if (newToken) {
+      console.log("probably a new non-scam token");
+    }
     if (scamScore <= 0) {
       console.log("scamScore = 1% ----> Probably not a scam");
     } else
@@ -42,5 +52,6 @@ function decide(infos) {
           "% < 50 ----> Probably not a scam"
       );
   }
+  return scamScore;
 }
 module.exports = decide;
