e092951300a1661c032165c322894767cf241a9b
Luka Samkharadze <lukesamkharadze@gmail.com>
2021-12-10 15:08:01 +0400

    feat: add rubic iframe

--- src/pages/CrossChainSwap/index.tsx
+++ src/pages/CrossChainSwap/index.tsx
@@ -1,24 +1,21 @@
 import React from 'react'
-import { Flex, Text } from '@summitswap-uikit'
-import CardNav from 'components/CardNav'
-import PageHeader from 'components/PageHeader'
-import { TranslateString } from 'utils/translateTextHelpers'
+import { Flex } from '@summitswap-uikit'
 import AppBody from '../AppBody'
-
-
 
 export default function CrossChainSwap() {
   return (
-    <AppBody>
-        <PageHeader
-          title={TranslateString(262, 'Liquidity')}
-          description={TranslateString(1168, 'Add liquidity to receive LP tokens')}
-        />
-      <Flex justifyContent="center">
-        <Text color="text" fontSize="20px">
-          Coming soon...
-        </Text>
-      </Flex>
-    </AppBody>
+    <Flex justifyContent="center">
+      <iframe
+        title="Rubic"
+        id="rubic-widget-iframe"
+        height="700px"
+        width="400px"
+        frameBorder="0"
+        src="https://app.rubic.exchange/?iframe=vertical&amp;amount=1&amp;background=%23011d2c&amp;bsc_tokens=%5B%220x8094e772fA4A60bdEb1DfEC56AB040e17DD608D5%22%5D&amp;from=ETH&amp;fromChain=ETH&amp;hideSelectionTo=true&amp;theme=dark&amp;to=KODA&amp;toChain=BSC"
+        style={{
+          borderRadius: 20,
+        }}
+      />
+    </Flex>
   )
 }
