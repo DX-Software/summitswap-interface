ed5c15224ab560a8c731079dec2af85bb41b6cbe
Luka Samkharadze <lukesamkharadze@gmail.com>
2022-01-07 15:17:10 +0400

    fix(wallet-connect): use function call

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -80,7 +80,7 @@ export default function App() {
 
   const handleLogin = (connectorId: string) => {
     if (connectorId === 'walletconnect') {
-      return activate(walletconnect)
+      return activate(walletconnect())
     }
     return activate(injected)
   }
