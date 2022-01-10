08c8ac5d41d78ddb412dd791c9fb20e8c5e31222
talhawaqar <talhawaqar007@gmail.com>
2022-01-10 14:54:05 +0500

    fix moonpay modal for mobile devices

--- src/components/swap/MoonpayModal.tsx
+++ src/components/swap/MoonpayModal.tsx
@@ -6,11 +6,11 @@ interface CustomModalProps extends InjectedModalProps {
 }
 
 const MoonpayModal: React.FC<CustomModalProps> = ({ title, onDismiss }) => (
-  <Modal title={title} onDismiss={onDismiss}>
+  <Modal title={title} onDismiss={onDismiss} bodyPadding='10px'>
     <iframe
       allow="accelerometer; autoplay; camera; gyroscope; payment"
       frameBorder="0"
-      height="500px"
+      height="600px"
       src="https://buy.moonpay.io?apiKey=pk_live_iPAKlFahWageWmf0LL4MjAWONzg55u&currencyCode=bnb_bsc&colorCode=%232ba55d"
       title="moonpay"
       style={{ borderRadius: "20px"}}
