67c0e9ccdcee3588e20f9322986040449ebc3b0d
erwin-huang <erwinhuank@gmail.com>
2022-01-22 12:45:03 +0700

    add routerAddress parameter for getRouterContract

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -88,8 +88,8 @@ export function getContract(address: string, ABI: any, library: Web3Provider, ac
 }
 
 // account is optional
-export function getRouterContract(_: number, library: Web3Provider, account?: string): Contract {
-  return getContract(ROUTER_ADDRESS, IUniswapV2Router02ABI, library, account)
+export function getRouterContract(_: number, library: Web3Provider, account?: string, routerAddress: string = ROUTER_ADDRESS): Contract {
+  return getContract(routerAddress, IUniswapV2Router02ABI, library, account)
 }
 
 export function escapeRegExp(string: string): string {
