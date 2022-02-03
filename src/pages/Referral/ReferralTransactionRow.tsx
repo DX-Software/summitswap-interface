0f75925c65110c5e71247ae4b990f9d0de9161a3
Luka Samkharadze <lukesamkharadze@gmail.com>
2022-02-03 18:29:23 +0400

    fix(swaplist); query over max amount of blocks and query in parallel

--- src/pages/Referral/ReferralTransactionRow.tsx
+++ src/pages/Referral/ReferralTransactionRow.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
 import { Flex, Text } from '@summitswap-uikit'
 import web3 from 'web3'
-import { useActiveWeb3React } from 'hooks'
-import { BigNumber, ethers } from 'ethers'
+import { ethers } from 'ethers'
 import { useWeb3React } from '@web3-react/core'
 import { useToken } from '../../hooks/Tokens'
+import { ReferralReward } from './types'
 
-interface Props {
-  timestamp: any
-  inputToken: string
-  outputToken: string
-  inputTokenAmount: BigNumber
-  outputTokenAmount: BigNumber
-  referrer: string
-  referrerReward: BigNumber
-  lead: string
-  leadReward: BigNumber
-}
-
-const ReferralTransactionRow: React.FC<Props> = ({
+const ReferralTransactionRow: React.FC<ReferralReward> = ({
   timestamp,
   inputToken: inputTokenAddress,
   outputToken: outputTokenAddress,
@@ -26,13 +14,11 @@ const ReferralTransactionRow: React.FC<Props> = ({
   outputTokenAmount,
   referrer,
   referrerReward,
-  lead,
   leadReward,
 }) => {
   const { account } = useWeb3React()
 
   const rewardAmount = account === referrer ? referrerReward : leadReward
-  // ethers.utils.formatUnits(balance, outputTokenAddress?.decimals)
 
   const date = `${new Date(web3.utils.hexToNumber(timestamp._hex) * 1000).getFullYear() - 2000}/${
     new Date(web3.utils.hexToNumber(timestamp._hex) * 1000).getMonth() + 1
@@ -48,8 +34,7 @@ const ReferralTransactionRow: React.FC<Props> = ({
         <span style={{ color: 'white' }}>{outputToken?.symbol} from Swap</span>{' '}
         {ethers.utils.formatUnits(inputTokenAmount, inputToken?.decimals)}{' '}
         <span style={{ color: 'white' }}>{inputToken?.symbol}</span>
-        <span style={{ color: 'white' }}> to</span>{' '}
-        {ethers.utils.formatUnits(outputTokenAmount, outputToken?.decimals)}{' '}
+        <span style={{ color: 'white' }}> to</span> {ethers.utils.formatUnits(outputTokenAmount, outputToken?.decimals)}{' '}
         <span style={{ color: 'white' }}>{outputToken?.symbol}</span>
       </Text>
     </Flex>
--- src/pages/Referral/SwapList.tsx
+++ src/pages/Referral/SwapList.tsx
@@ -3,35 +3,61 @@ import { useReferralContract } from 'hooks/useContract'
 import _ from 'lodash'
 import React, { useState, useEffect } from 'react'
 import { Box, Text } from '@summitswap-uikit'
-import { REFERRAL_ADDRESS, REFERRAL_DEPLOYMENT_BLOCKNUMBER } from '../../constants'
+import { Event } from 'ethers'
+import { MAX_QUERYING_BLOCK_AMOUNT, REFERRAL_ADDRESS, REFERRAL_DEPLOYMENT_BLOCKNUMBER } from '../../constants'
 import ReferralTransactionRow from './ReferralTransactionRow'
+import { ReferralReward } from './types'
 
 export default function SwapList() {
-  const { account } = useActiveWeb3React()
+  const { account, library } = useActiveWeb3React()
   const referralContract = useReferralContract(REFERRAL_ADDRESS, true)
 
   const [swapList, setSwapList] = useState<any[]>([])
 
   useEffect(() => {
+    setSwapList([])
+  }, [account])
+
+  useEffect(() => {
     async function fetchSwapList() {
-      if (!account || !referralContract) return
+      if (!account || !referralContract || !library) return
 
       const referrerFilter = referralContract.filters.ReferralReward(account)
       const leadFilter = referralContract.filters.ReferralReward(null, account)
 
-      const referrerLogs = await referralContract.queryFilter(referrerFilter, REFERRAL_DEPLOYMENT_BLOCKNUMBER, 'latest')
-      const leadLogs = await referralContract.queryFilter(leadFilter, REFERRAL_DEPLOYMENT_BLOCKNUMBER, 'latest')
+      const latestBlocknumber = await library.getBlockNumber()
 
-      let eventLogs: Array<any> = [...referrerLogs, ...leadLogs]
-      eventLogs = eventLogs.map((eventLog) => eventLog.args)
-      eventLogs = _.orderBy(eventLogs, (eventLog) => eventLog.timestamp.toNumber(), 'desc')
+      let referrerLogs = [] as Event[]
+      let leadLogs = [] as Event[]
 
-      // console.log(eventLogs)
-      setSwapList(eventLogs)
+      const queries: [start: number, end: number][] = []
+
+      for (
+        let blockNumber = REFERRAL_DEPLOYMENT_BLOCKNUMBER;
+        blockNumber < latestBlocknumber;
+        blockNumber += MAX_QUERYING_BLOCK_AMOUNT
+      ) {
+        queries.push([blockNumber, Math.min(latestBlocknumber, blockNumber + MAX_QUERYING_BLOCK_AMOUNT - 1)])
+      }
+
+      await Promise.all(
+        queries.map(async (o) => {
+          const referrerLogsOnInterval = await referralContract.queryFilter(referrerFilter, o[0], o[1])
+          const leadLogsOnInterval = await referralContract.queryFilter(leadFilter, o[0], o[1])
+
+          referrerLogs = [...referrerLogs, ...referrerLogsOnInterval]
+          leadLogs = [...leadLogs, ...leadLogsOnInterval]
+
+          let eventLogs = [...referrerLogs, ...leadLogs].map((oo) => oo.args) as ReferralReward[]
+          eventLogs = _.orderBy(eventLogs, (eventLog) => eventLog.timestamp.toNumber(), 'desc')
+
+          setSwapList(eventLogs)
+        })
+      )
     }
 
     fetchSwapList()
-  }, [account, referralContract])
+  }, [account, library, referralContract])
 
   return (
     <>
@@ -41,8 +67,8 @@ export default function SwapList() {
             Referred swaps
           </Text>
           <Box mb={2}>
-            {_.map(swapList, (x: any) => (
-              <ReferralTransactionRow account={account} {...x} />
+            {_.map(swapList, (swap: any) => (
+              <ReferralTransactionRow account={account} {...swap} />
             ))}
           </Box>
         </>
