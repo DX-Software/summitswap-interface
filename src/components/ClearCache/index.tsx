e74146d26a155c163f3994b5d542b107b0a7ddf1
Luka Samkharadze <lukesamkharadze@gmail.com>
2022-02-08 14:05:54 +0400

    refactor(clearCache): remove uncessesary function

--- src/components/ClearCache/index.tsx
+++ src/components/ClearCache/index.tsx
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from "react"
-import packageJson from "../../../package.json"
+import React, { useState, useEffect } from 'react'
+import packageJson from '../../../package.json'
 
-function buildDateGreaterThan(latestDate, currentDate): boolean {
-  const momLatestDateTime = BigInt(latestDate)
-  const momCurrentDateTime = BigInt(currentDate)
-
-  return momLatestDateTime > momCurrentDateTime
+function refreshCacheAndReload() {
+  if (caches) {
+    // Service worker cache should be cleared with caches.delete()
+    caches.keys().then((names) => {
+      names.forEach((name) => {
+        caches.delete(name)
+      })
+    })
+  }
+  // delete browser cache and hard reload
+  // eslint-disable-next-line
+  window.location.href = window.location.href
 }
 
 function withClearCache(Component) {
@@ -13,16 +20,14 @@ function withClearCache(Component) {
     const [isLatestBuildDate, setIsLatestBuildDate] = useState(false)
 
     useEffect(() => {
-      fetch("/meta.json")
+      fetch('/meta.json')
         .then((response) => response.json())
         .then((meta) => {
-          const latestVersionDate = meta.buildDate
-          const currentVersionDate = packageJson.buildDate
+          const latestVersionDate = meta.buildDate as number
+          const currentVersionDate = packageJson.buildDate as number
 
-          const shouldForceRefresh = buildDateGreaterThan(
-            latestVersionDate,
-            currentVersionDate
-          )
+          const shouldForceRefresh = latestVersionDate > currentVersionDate
+
           if (shouldForceRefresh) {
             setIsLatestBuildDate(false)
             refreshCacheAndReload()
@@ -32,25 +37,7 @@ function withClearCache(Component) {
         })
     }, [])
 
-    const refreshCacheAndReload = () => {
-      if (caches) {
-        // Service worker cache should be cleared with caches.delete()
-        caches.keys().then((names) => {
-          names.forEach((name) => {
-            caches.delete(name)
-          })
-        })
-      }
-      // delete browser cache and hard reload
-      // eslint-disable-next-line
-      window.location.href = window.location.href
-    }
-
-    return (
-      <>
-        {isLatestBuildDate ? <Component {...props} /> : null}
-      </>
-    )
+    return <>{isLatestBuildDate ? <Component {...props} /> : null}</>
   }
 
   return ClearCacheComponent
