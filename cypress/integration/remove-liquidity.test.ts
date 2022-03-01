b8f0986434c87d99ea78f9579519cd234ebdf62d
erwin-huang <erwinhuank@gmail.com>
2022-03-01 19:25:48 +0700

    fix add liquidity tests and remove xit

--- cypress/integration/remove-liquidity.test.ts
+++ cypress/integration/remove-liquidity.test.ts
@@ -7,31 +7,31 @@ describe('Remove Liquidity', () => {
     )
   })
 
-  xit('bnb remove', () => {
+  it('bnb remove', () => {
     cy.visit('/remove/ETH/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82')
     cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'BNB')
     cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'CAKE')
   })
 
-  xit('bnb remove swap order', () => {
+  it('bnb remove swap order', () => {
     cy.visit('/remove/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82/ETH')
     cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'CAKE')
     cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'BNB')
   })
 
-  xit('loads the two correct tokens', () => {
+  it('loads the two correct tokens', () => {
     cy.visit('/remove/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82-0xe9e7cea3dedca5984780bafc599bd69add087d56')
     cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'CAKE')
     cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'BUSD')
   })
 
-  xit('does not crash if CAKE is duplicated', () => {
+  it('does not crash if CAKE is duplicated', () => {
     cy.visit('/remove/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82-0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82')
     cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'CAKE')
     cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'CAKE')
   })
 
-  xit('token not in storage is loaded', () => {
+  it('token not in storage is loaded', () => {
     cy.visit('/remove/0x7083609fce4d1d8dc0c979aab8c869ea2c873402-0x2170ed0880ac9a755fd29b2688956bd959f933f8')
     cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'DOT')
     cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'ETH')
