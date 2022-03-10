7cf83c33a313d84f24a42b1dbc454861059c602c
erwin-huang <erwinhuank@gmail.com>
2022-03-10 13:53:08 +0700

    update multicall address to multicall v2 address

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@koda-finance/summitswap-sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
+  [ChainId.MAINNET]: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B', // TODO
   [ChainId.BSCTESTNET]: `${process.env.REACT_APP_MULTI_CALL_TEST}`,
 }
 
