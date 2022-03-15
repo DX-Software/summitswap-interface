add223de02600b3d07111cd5c227177160688306
Omer Furkan <omerfurkansen@gmail.com>
2022-03-15 16:58:42 +0300

    indention is changed to 2 from 4

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,31 +1,30 @@
 import styled from 'styled-components';
 
 const Banner = styled.div`
-    background-color: gray;
-    border-radius: 10px;
-    margin-top: 20px;   
-    width: 970px;
-    height: 90px;
+  background-color: gray;
+  border-radius: 10px;
+  margin-top: 20px;   
+  width: 970px;
+  height: 90px;
 
-    @media (max-width: 1230px) {
-        & {
-            width: 94%;
-        }
+  @media (max-width: 1230px) {
+    & {
+      width: 94%;
     }
+  }
 
-    @media (max-width: 968px) {
-        & {
-            max-width: 780px;
-        }
+  @media (max-width: 968px) {
+    & {
+      max-width: 780px;
     }
+  }
 
-    @media (max-width: 440px) {
-        & {
-            max-width: 320px;
-            height: 100px
-        }
+  @media (max-width: 440px) {
+    & {
+      max-width: 320px;
+      height: 100px
     }
-    
+  }
 `
 
 export default Banner;
