792563322ad3acc7ba39fbd4fdd0bab1574ac477
lukesamkharadze <lukesamkharadze@gmail.com>
2022-03-25 21:24:44 +0400

    fix(onboarding): addliquidity redirection to swap page

--- src/pages/Onboarding/AddLiquidity.tsx
+++ src/pages/Onboarding/AddLiquidity.tsx
@@ -3,8 +3,7 @@ import { Link } from 'react-router-dom'
 import { Button, Text } from '@koda-finance/summitswap-uikit'
 import { Token } from '@koda-finance/summitswap-sdk'
 import { useWeb3React } from '@web3-react/core'
-import { abbreviateNumber } from 'utils'
-import { KODA, MINIMUM_KODA_FOR_ONBOARDING } from '../../constants'
+import { KODA } from '../../constants'
 
 interface Props {
   token: Token | undefined
@@ -18,12 +17,11 @@ export default function AddLiquidity({ token, isLoading, isEnoughLiquidity }: Pr
   return (
     <article>
       <p>
-        Add liquidity on <b>KODA/{token?.symbol ?? 'YOUR COIN'}</b>. Minimum{' '}
-        <b>{abbreviateNumber(MINIMUM_KODA_FOR_ONBOARDING)} KODA</b>
+        Add liquidity on <b>KODA/{token?.symbol ?? 'YOUR TOKEN'}</b>
       </p>
       {token && account && (
         <>
-          <Button as={Link} to={`/add/${KODA}/${token?.address}`} disabled={isLoading}>
+          <Button as={Link} to={`/add/${KODA.address}/${token?.address}`} disabled={isLoading}>
             Add Liquidity
           </Button>
           <p>{!isEnoughLiquidity && <Text color="red">Not enough liquidity, please add more</Text>}</p>
