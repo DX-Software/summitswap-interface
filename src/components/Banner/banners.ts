c93ee23931256eefc2cf5f9c935269b73df89fde
Omer Furkan <omerfurkansen@gmail.com>
2022-03-26 02:27:20 +0300

    feat: display random ad

---
+++ src/components/Banner/banners.ts
@@ -0,0 +1,22 @@
+import KapexSmallBanner from '../../img/banners/kapex_small.gif'
+import KapexMediumBanner from '../../img/banners/kapex_medium.gif'
+import KapexLargeBanner from '../../img/banners/kapex_large.gif'
+import ExampleBannerLarge1 from '../../img/banners/example1.gif'
+import ExampleBannerLarge2 from '../../img/banners/example2.gif'
+import ExampleBannerLarge3 from '../../img/banners/example3.gif'
+import ExampleBannerLarge4 from '../../img/banners/example4.gif'
+import ExampleBannerMedium1 from '../../img/banners/examplem1.gif'
+import ExampleBannerMedium2 from '../../img/banners/examplem2.gif'
+import ExampleBannerMedium3 from '../../img/banners/examplem3.gif'
+import ExampleBannerMedium4 from '../../img/banners/examplem4.gif'
+
+
+const BANNERS = [
+    [KapexLargeBanner, KapexMediumBanner, KapexSmallBanner], 
+    [ExampleBannerLarge1, ExampleBannerMedium1, ""],
+    [ExampleBannerLarge2, ExampleBannerMedium2, ""],
+    [ExampleBannerLarge3, ExampleBannerMedium3, ""],
+    [ExampleBannerLarge4, ExampleBannerMedium4, ""],
+]
+
+export default BANNERS;
