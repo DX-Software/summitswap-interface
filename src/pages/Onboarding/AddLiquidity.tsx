4483d7b2d0ed2632f6ca913f85396bf9de86d4b6
lukesamkharadze <lukesamkharadze@gmail.com>
2022-03-25 11:02:59 +0400

    feat(onboarding): add liquidity component

---
+++ src/pages/Onboarding/AddLiquidity.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Text } from '@koda-finance/summitswap-uikit'
+import { Token } from '@koda-finance/summitswap-sdk'
+import { useWeb3React } from '@web3-react/core'
+import { abbreviateNumber } from 'utils'
+import { KODA, MINIMUM_KODA_FOR_ONBOARDING } from '../../constants'
+
+interface Props {
+  token: Token | undefined
+  isLoading: boolean
+  isEnoughLiquidity: boolean
+}
+
+export default function AddLiquidity({ token, isLoading, isEnoughLiquidity }: Props) {
+  const { account } = useWeb3React()
+
+  return (
+    <>
+      <p className="paragraph">
+        1. Add liquidity on <b>KODA/{token?.symbol ?? 'YOUR COIN'}</b>. Minimum{' '}
+        <b>{abbreviateNumber(MINIMUM_KODA_FOR_ONBOARDING)} KODA</b>
+      </p>
+      {token && account ? (
+        <>
+          <Button as={Link} to={`/add/${KODA}/${token?.address}`} disabled={isLoading}>
+            Add Liquidity
+          </Button>
+          <p className="paragraph">
+            {!isEnoughLiquidity && <Text color="red">Not enough liquidity, please add more</Text>}
+          </p>
+        </>
+      ) : (
+        <></>
+      )}
+    </>
+  )
+}
