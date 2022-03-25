1d937a6c83d8e0d254fbf25384f2ad726bd5b7f7
lukesamkharadze <lukesamkharadze@gmail.com>
2022-03-25 16:27:19 +0400

    feat(onboarding): add submit component

---
+++ src/pages/Onboarding/Submit.tsx
@@ -0,0 +1,79 @@
+import { Token } from '@koda-finance/summitswap-sdk'
+import { Button, useModal } from '@koda-finance/summitswap-uikit'
+import { useWeb3React } from '@web3-react/core'
+import axios from 'axios'
+import React, { useCallback } from 'react'
+import { ONBOARDING_API } from '../../constants'
+import SuccessModal from './SuccessModal'
+
+interface Props {
+  token: Token | undefined
+  isTokensSentToReferral: boolean
+  firstBuyPercentage: string | undefined
+  referrerPercentage: string | undefined
+  isReferralContractRemovedFromFees: boolean
+  isLoading: boolean
+  pairAddress: string | undefined
+  fetchUserLocked: any
+}
+
+export default function Submit({
+  token,
+  isTokensSentToReferral,
+  firstBuyPercentage,
+  referrerPercentage,
+  isReferralContractRemovedFromFees,
+  isLoading,
+  pairAddress,
+  fetchUserLocked,
+}: Props) {
+  const { account } = useWeb3React()
+
+  const [displaySucessModal] = useModal(<SuccessModal title="Success" />)
+
+  const submit = useCallback(() => {
+    async function submitToken() {
+      if (!firstBuyPercentage || !referrerPercentage || !token || !pairAddress || !account) {
+        return
+      }
+
+      const { fetchedLpLocks, totalAmountOfLpLocked } = await fetchUserLocked()
+
+      await axios.post(ONBOARDING_API, {
+        message: `
+          Token: ${token.address}
+          %0AUser: ${account}
+          %0APair: ${pairAddress}
+          %0ALockIds: ${fetchedLpLocks?.map((o) => o.lockId)}
+          %0ATotalLocked: ${totalAmountOfLpLocked}
+          %0AReferrer Fee: ${referrerPercentage}
+          %0AFirst Buy Fee: ${firstBuyPercentage}`,
+      })
+
+      displaySucessModal()
+    }
+
+    submitToken()
+  }, [firstBuyPercentage, referrerPercentage, token, pairAddress, account, fetchUserLocked, displaySucessModal])
+
+  return (
+    <>
+      {token && account && (
+        <Button
+          disabled={
+            !isTokensSentToReferral ||
+            !firstBuyPercentage ||
+            !referrerPercentage ||
+            !isReferralContractRemovedFromFees ||
+            isLoading ||
+            +(firstBuyPercentage ?? '') <= 0 ||
+            +(referrerPercentage ?? '') <= 0
+          }
+          onClick={submit}
+        >
+          Submit
+        </Button>
+      )}
+    </>
+  )
+}
