575827677468f9f75735178a881dc7208d738c86
lukesamkharadze <lukesamkharadze@gmail.com>
2022-03-25 13:11:58 +0400

    feat(onboarding): add sendreferralreward component

---
+++ src/pages/Onboarding/SendReferralRewards.tsx
@@ -0,0 +1,83 @@
+import { Token } from '@koda-finance/summitswap-sdk'
+import { Button, Input, Text } from '@koda-finance/summitswap-uikit'
+import { useWeb3React } from '@web3-react/core'
+import { Contract, ethers } from 'ethers'
+import React, { Dispatch, SetStateAction, useCallback, useState } from 'react'
+import { REFERRAL_ADDRESS } from '../../constants'
+
+interface Props {
+  token: Token | undefined
+  tokenContract: Contract | null
+  isLoading: boolean
+  setIsLoading: Dispatch<SetStateAction<boolean>>
+  setIsTokensSentToReferral: Dispatch<SetStateAction<boolean>>
+  isLiquidityLocked: boolean
+}
+
+export default function SendReferralRewards({
+  token,
+  tokenContract,
+  isLoading,
+  isLiquidityLocked,
+  setIsTokensSentToReferral,
+  setIsLoading,
+}: Props) {
+  const { account, library } = useWeb3React()
+
+  const [referralRewardAmount, setReferralRewardAmount] = useState<string>()
+
+  const sendTokensToReferralContract = useCallback(() => {
+    async function send() {
+      if (!tokenContract || !referralRewardAmount || !library) {
+        await Promise.resolve()
+        setIsTokensSentToReferral(false)
+        return
+      }
+
+      if (parseInt(referralRewardAmount) <= 0) {
+        return
+      }
+
+      const receipt = await tokenContract.transfer(REFERRAL_ADDRESS, ethers.utils.parseEther(referralRewardAmount))
+
+      setIsLoading(true)
+      await library.waitForTransaction(receipt.hash)
+      setIsLoading(false)
+
+      setIsTokensSentToReferral(true)
+    }
+
+    send()
+  }, [tokenContract, referralRewardAmount, library, setIsLoading, setIsTokensSentToReferral])
+
+  return (
+    <article>
+      <p>
+        3. Send some of <b>{token?.symbol ?? 'YOUR TOKEN'}</b> to the referral contract for referral rewards
+        <br />
+      </p>
+      {token && account && (
+        <>
+          <Input
+            disabled={!isLiquidityLocked || isLoading}
+            type="number"
+            placeholder="Enter token amount"
+            onChange={(o) => setReferralRewardAmount(o.target.value)}
+            style={{ marginTop: '10px', marginBottom: '10px' }}
+          />
+          <Button
+            disabled={!isLiquidityLocked || isLoading || (parseInt(referralRewardAmount ?? '') || 0) <= 0}
+            onClick={sendTokensToReferralContract}
+          >
+            Transfer
+          </Button>
+          <p>
+            {!(parseInt(referralRewardAmount || '') > 0) && isLiquidityLocked && (
+              <Text color="red">❌ Please enter positive number</Text>
+            )}
+          </p>
+        </>
+      )}
+    </article>
+  )
+}
