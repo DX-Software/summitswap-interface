5fb1988ec6501a94a1643beb84b94a76be0a202a
lukesamkharadze <lukesamkharadze@gmail.com>
2022-03-31 20:26:47 +0400

    fix(tokenCard): use exported kapex instead of kapexs

--- src/pages/Referral/TokenCard.tsx
+++ src/pages/Referral/TokenCard.tsx
@@ -70,7 +70,7 @@ const TokenCard: React.FC<Props> = ({ tokenAddress, hasClaimedAll, isLoading, se
     if (!outputToken) return
     if (!rewardToken) return
 
-    const tokenList: Token[] = [BUSDs[CHAIN_ID], KAPEXs[CHAIN_ID], outputToken, rewardToken].filter(o => !!o && o !== NULL_ADDRESS);
+    const tokenList: Token[] = [BUSDs[CHAIN_ID], KAPEX, outputToken, rewardToken].filter(o => !!o && o !== NULL_ADDRESS);
     const uniqueTokenAddresses = [...new Set(tokenList.map((o) => o.address))]
     const uniqueTokenList = uniqueTokenAddresses.map((o) => tokenList.find((oo) => oo.address === o)) as Token[]
 
