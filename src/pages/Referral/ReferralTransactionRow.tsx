c696cb547e335a87cc070b1d381acf3c2c6db808
erwin-huang <erwinhuank@gmail.com>
2022-04-21 12:35:26 +0700

    fix(referralRewardHistories): apply subgraph for referral reward histories

--- src/pages/Referral/ReferralTransactionRow.tsx
+++ src/pages/Referral/ReferralTransactionRow.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Flex, Text } from '@koda-finance/summitswap-uikit'
 import web3 from 'web3'
-import { ethers } from 'ethers'
+import { ethers, BigNumber } from 'ethers'
 import { useWeb3React } from '@web3-react/core'
 import { useToken } from '../../hooks/Tokens'
 import { ReferralReward } from './types'
@@ -9,7 +9,9 @@ import { ReferralReward } from './types'
 const ReferralTransactionRow: React.FC<ReferralReward> = ({
   timestamp,
   inputToken: inputTokenAddress,
+  inputTokenSymbol,
   outputToken: outputTokenAddress,
+  outputTokenSymbol,
   inputTokenAmount,
   outputTokenAmount,
   referrer,
@@ -18,24 +20,21 @@ const ReferralTransactionRow: React.FC<ReferralReward> = ({
 }) => {
   const { account } = useWeb3React()
 
-  const rewardAmount = account === referrer ? referrerReward : leadReward
+  const rewardAmount = account?.toLowerCase() === referrer ? referrerReward : leadReward
 
-  const date = `${new Date(web3.utils.hexToNumber(timestamp._hex) * 1000).getFullYear() - 2000}/${
-    new Date(web3.utils.hexToNumber(timestamp._hex) * 1000).getMonth() + 1
-  }/${new Date(web3.utils.hexToNumber(timestamp._hex) * 1000).getDate()}`
-
-  const inputToken = useToken(inputTokenAddress)
-  const outputToken = useToken(outputTokenAddress)
+  const date = `${new Date(Number(timestamp) * 1000).getFullYear() - 2000}/${
+    new Date(Number(timestamp) * 1000).getMonth() + 1
+  }/${new Date(Number(timestamp) * 1000).getDate()}`
 
   return (
     <Flex alignItems="center" justifyContent="space-between">
       <Text mb="2px" color="primary">
-        <span style={{ color: 'white' }}>{date}</span> {ethers.utils.formatUnits(rewardAmount, outputToken?.decimals)}{' '}
-        <span style={{ color: 'white' }}>{outputToken?.symbol} from Swap</span>{' '}
-        {ethers.utils.formatUnits(inputTokenAmount, inputToken?.decimals)}{' '}
-        <span style={{ color: 'white' }}>{inputToken?.symbol}</span>
-        <span style={{ color: 'white' }}> to</span> {ethers.utils.formatUnits(outputTokenAmount, outputToken?.decimals)}{' '}
-        <span style={{ color: 'white' }}>{outputToken?.symbol}</span>
+        <span style={{ color: 'white' }}>{date}</span> {rewardAmount}{' '}
+        <span style={{ color: 'white' }}>{outputTokenSymbol} from Swap</span>{' '}
+        {inputTokenAmount}{' '}
+        <span style={{ color: 'white' }}>{inputTokenSymbol}</span>
+        <span style={{ color: 'white' }}> to</span> {outputTokenAmount}{' '}
+        <span style={{ color: 'white' }}>{outputTokenSymbol}</span>
       </Text>
     </Flex>
   )
--- src/pages/Referral/SwapList.tsx
+++ src/pages/Referral/SwapList.tsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { Box, Text } from '@koda-finance/summitswap-uikit'
 import _ from 'lodash'
-import { Event } from 'ethers'
-import CustomLightSpinner from '../../components/CustomLightSpinner'
-import { useReferralContract } from '../../hooks/useContract'
+import useReferralHistories from 'hooks/useReferralHistories'
 import { useActiveWeb3React } from '../../hooks'
-import { MAX_QUERYING_BLOCK_AMOUNT, REFERRAL_DEPLOYMENT_BLOCKNUMBER } from '../../constants'
 import ReferralTransactionRow from './ReferralTransactionRow'
 import { ReferralReward } from './types'
 import RewardedTokens from './RewardedTokens'
@@ -13,59 +10,8 @@ import RewardedTokens from './RewardedTokens'
 
 
 export default function SwapList() {
-  const { account, library } = useActiveWeb3React()
-  const referralContract = useReferralContract(true)
-
-  const [swapList, setSwapList] = useState<ReferralReward[]>([])
-  const [isLoading, setIsLoading] = useState(false)
-
-  useEffect(() => {
-    setSwapList([])
-  }, [account])
-
-  useEffect(() => {
-    async function fetchSwapList() {
-      if (!account || !referralContract || !library) return
-
-      const referrerFilter = referralContract.filters.ReferralReward(account)
-      const leadFilter = referralContract.filters.ReferralReward(null, account)
-
-      const latestBlocknumber = await library.getBlockNumber()
-
-      let referrerLogs = [] as Event[]
-      let leadLogs = [] as Event[]
-
-      const queries: [start: number, end: number][] = []
-
-      for (
-        let blockNumber = REFERRAL_DEPLOYMENT_BLOCKNUMBER;
-        blockNumber < latestBlocknumber;
-        blockNumber += MAX_QUERYING_BLOCK_AMOUNT
-      ) {
-        queries.push([blockNumber, Math.min(latestBlocknumber, blockNumber + MAX_QUERYING_BLOCK_AMOUNT - 1)])
-      }
-
-      for (let i = 0; i < queries.length; i++) {
-        try {
-          const referrerLogsOnInterval = await referralContract.queryFilter(referrerFilter, queries[i][0], queries[i][1])
-          const leadLogsOnInterval = await referralContract.queryFilter(leadFilter, queries[i][0], queries[i][1])
-  
-          referrerLogs = [...referrerLogs, ...referrerLogsOnInterval]
-          leadLogs = [...leadLogs, ...leadLogsOnInterval]
-  
-          let eventLogs = [...referrerLogs, ...leadLogs].map((oo) => oo.args) as ReferralReward[]
-          eventLogs = _.orderBy(eventLogs, (eventLog) => eventLog.timestamp.toNumber(), 'desc')
-  
-          setSwapList(eventLogs)
-        } catch (err) {
-          console.log("Error: ", err)
-        }
-      }
-      setIsLoading(false)
-    }
-
-    // fetchSwapList()
-  }, [account, library, referralContract])
+  const { account } = useActiveWeb3React()
+  const referralHistories = useReferralHistories(account ?? "")
 
   return (
     <>
@@ -73,17 +19,15 @@ export default function SwapList() {
         Referred swap histories
       </Text>
       <Text> Section under maintenance, please try again later </Text>
-      {/* <Box mb={2}>
-        {_.map(swapList, (swap: ReferralReward) => (
-          <ReferralTransactionRow key={`${swap.timestamp}`} account={account} {...swap} />
+      <Box mb={2}>
+        {_.map(referralHistories, (referralHistory: ReferralReward) => (
+          <ReferralTransactionRow key={`${referralHistory.timestamp}`} account={account} {...referralHistory} />
         ))}
       </Box>
-      {isLoading && (
-        <CustomLightSpinner src="/images/blue-loader.svg" alt="loader" size="45px" />
-      )}
-      {(swapList.length === 0 && !isLoading) && (
+      {referralHistories.length}
+      {(referralHistories.length === 0) && (
         <Text bold> No history to be displayed! </Text>
-      )} */}
+      )}
       <RewardedTokens />
     </>
   )
