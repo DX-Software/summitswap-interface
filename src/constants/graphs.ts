35fb311930ae555a3e81582f896861c0f0c5f725
erwin-huang <erwinhuank@gmail.com>
2022-04-22 14:35:10 +0700

    fix: update src/constants/graphs.ts code style

--- src/constants/graphs.ts
+++ src/constants/graphs.ts
@@ -1,2 +1,3 @@
-export const REFERRAL_CLIENT = process.env.REACT_APP_LINK_GRAPH_NODE_REFERRAL ?? "";
-export const BLOCK_CLIENT = process.env.REACT_APP_LINK_GRAPH_NODE_BLOCK ?? "";
+export const REFERRAL_CLIENT = `${process.env.REACT_APP_LINK_GRAPH_NODE_REFERRAL}`
+export const BLOCK_CLIENT = `${process.env.REACT_APP_LINK_GRAPH_NODE_BLOCK}`
+
