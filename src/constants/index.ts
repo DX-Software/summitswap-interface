54a2b4e1ff0aee6c8aa5525125e70c8b2d71a4fc
lukesamkharadze <lukesamkharadze@gmail.com>
2022-04-28 01:06:48 +0400

    fix: update koda testnet address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -34,7 +34,7 @@ type ChainTokenList = {
 
 export const KODA = {
   [ChainId.MAINNET]: new Token(ChainId.MAINNET, '0x8094e772fA4A60bdEb1DfEC56AB040e17DD608D5', 9, 'KODA', 'KODA Token', 100, true, 11.25, 11.23),
-  [ChainId.BSCTESTNET]: new Token(ChainId.BSCTESTNET, '0x063646d9C4eCB1c341bECdEE162958f072C43561', 9, 'KODA', 'KODA Token', 100, true, 11.25, 11.23)
+  [ChainId.BSCTESTNET]: new Token(ChainId.BSCTESTNET, '0xa3A654C2369277237372cCce51Ca8403acB4FAe4', 9, 'KODA', 'KODA Token', 100, true, 11.25, 11.23)
 }[CHAIN_ID] as Token
 export const DAI = new Token(ChainId.MAINNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
 export const USDT = new Token(ChainId.MAINNET, '0x55d398326f99059fF775485246999027B3197955', 18, 'USDT', 'Tether USD')
