b32ffe1c74a39f18f41a5dbe998ca4f074f4a50f
erwin-huang <erwinhuank@gmail.com>
2022-04-29 08:58:38 +0700

    feat(SwapList): add validation for network support

--- src/pages/Referral/SwapList.tsx
+++ src/pages/Referral/SwapList.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Box, Text } from '@koda-finance/summitswap-uikit'
 import _ from 'lodash'
 import useReferralHistories from 'hooks/useReferralHistories'
+import { REFERRAL_CLIENT } from 'constants/graphs'
 import { useActiveWeb3React } from '../../hooks'
 import ReferralTransactionRow from './ReferralTransactionRow'
 import { ReferralReward } from './types'
@@ -18,14 +19,18 @@ export default function SwapList() {
       <Text bold mb={2}>
         Referred swap history
       </Text>
-      <Box mb={2}>
-        {_.map(referralHistories, (referralHistory: ReferralReward) => (
-          <ReferralTransactionRow key={`${referralHistory.id}-${referralHistory.timestamp}`} account={account} {...referralHistory} />
-        ))}
-      </Box>
-      {(referralHistories.length === 0) && (
-        <Text bold> No history to be displayed! </Text>
-      )}
+      {REFERRAL_CLIENT === ""
+        ? <Text bold>Swap history is not supported on current network!</Text>
+        : (referralHistories.length === 0)
+        ? <Text bold> No history to be displayed!</Text>
+        : (
+          <Box mb={2}>
+            {_.map(referralHistories, (referralHistory: ReferralReward) => (
+              <ReferralTransactionRow key={`${referralHistory.id}-${referralHistory.timestamp}`} account={account} {...referralHistory} />
+            ))}
+          </Box>
+        )
+      }
       <RewardedTokens />
     </>
   )
