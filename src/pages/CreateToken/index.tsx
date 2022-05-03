68078c44571d5533f7994a5e0a2a378bb937bd3c
jmontanholi <jonunes9326@hotmail.com.br>
2022-05-03 12:28:30 -0300

    Last version of factoryContract

--- src/pages/CreateToken/index.tsx
+++ src/pages/CreateToken/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { ethers } from '@ethersproject/experimental/node_modules/ethers';
 import AppBody from '../AppBody';
