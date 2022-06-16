ef47c7dca2e95e1ec19763c150a5a1b62c7b9414
umaismateen <umais.mateen@gmail.com>
2022-06-16 14:44:41 +0500

    Endgame banners

--- src/components/Banner/banners.ts
+++ src/components/Banner/banners.ts
@@ -4,6 +4,9 @@ import KapexLargeBanner from '../../img/banners/kapex/kapex_large_v3.gif'
 import KodaSmallBanner from '../../img/banners/koda/koda_small_v2.gif'
 import KodaMediumBanner from '../../img/banners/koda/koda_medium_v2.gif'
 import KodaLargeBanner from '../../img/banners/koda/koda_large_v2.gif'
+import EndgameSmallBanner from '../../img/banners/endgame/endgame_small.gif'
+import EndgameMediumBanner from '../../img/banners/endgame/endgame_medium.gif'
+import EndgameLargeBanner from '../../img/banners/endgame/endgame_large.gif'
 
 const BANNERS = {
     kapex: {
@@ -15,7 +18,13 @@ const BANNERS = {
         gifs: [KodaLargeBanner, KodaMediumBanner, KodaSmallBanner],
         link: "https://koda.finance/",
         delay: 6200
-    }
+    },
+    endgame: {
+        gifs: [EndgameLargeBanner, EndgameMediumBanner, EndgameSmallBanner],
+        link: "https://summitswap.finance/#/swap?output=0x373e4b4E4D328927bc398A9B50e0082C6f91B7bb",
+        delay: 5200
+    },
+
 }
 
 export default BANNERS
