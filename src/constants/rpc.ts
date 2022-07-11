4874ce8085f2bdf40bbc43d8997ce1b8323651d8
lukesamkharadze <lukesamkharadze@gmail.com>
2022-07-11 21:35:55 +0200

    feat(avax): add rpc urls

---
+++ src/constants/rpc.ts
@@ -0,0 +1,8 @@
+import { ChainId } from './chainId'
+
+export const RPC = {
+  [ChainId.BSC]: 'https://bsc-dataseed.binance.org/',
+  [ChainId.BSC_TESTNET]: 'https://data-seed-prebsc-2-s3.binance.org:8545',
+  [ChainId.AVALANCHE]: 'https://api.avax.network/ext/bc/C/rpc',
+  [ChainId.AVALANCHE_TESTNET]: 'https://api.avax-test.network/ext/bc/C/rpc',
+}
