92a807a74708e7bf937d593585cb1cedb18f935d
lukesamkharadze <lukesamkharadze@gmail.com>
2022-07-13 15:43:12 +0200

    feat(avax): add other evm chains

--- src/constants/currenyLogos.ts
+++ src/constants/currenyLogos.ts
@@ -2,10 +2,51 @@ import { ChainId } from "./chainId"
 
 const AvalancheLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/avax.jpg'
 const BinanceCoinLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/bnb.jpg'
+const EthereumLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/eth.jpg'
+const FantomLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/ftm.jpg'
+const HarmonyLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/one.jpg'
+const HecoLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/heco.jpg'
+const MaticLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/polygon.jpg'
+const MoonbeamLogo = 'https://raw.githubusercontent.com/sushiswap/icons/master/network/moonbeam.jpg'
+const OKExLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/okt.jpg'
+const xDaiLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/xdai.jpg'
+const CeloLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/celo.jpg'
+const PalmLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/palm.jpg'
+const MovrLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/movr.jpg'
+const FuseLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/fuse.jpg'
+const TelosLogo = 'https://raw.githubusercontent.com/sushiswap/logos/main/token/telos.jpg'
 
 export const NETWORK_LOGOS: Record<ChainId, string> = {
+  [ChainId.ETHEREUM]: EthereumLogo,
+  [ChainId.KOVAN]: EthereumLogo,
+  [ChainId.RINKEBY]: EthereumLogo,
+  [ChainId.ROPSTEN]: EthereumLogo,
+  [ChainId.GÖRLI]: EthereumLogo,
+  [ChainId.FANTOM]: FantomLogo,
+  [ChainId.FANTOM_TESTNET]: FantomLogo,
+  [ChainId.MATIC]: MaticLogo,
+  [ChainId.MATIC_TESTNET]: MaticLogo,
+  [ChainId.XDAI]: xDaiLogo,
   [ChainId.BSC]: BinanceCoinLogo,
   [ChainId.BSC_TESTNET]: BinanceCoinLogo,
+  [ChainId.MOONBEAM_TESTNET]: MoonbeamLogo,
   [ChainId.AVALANCHE]: AvalancheLogo,
   [ChainId.AVALANCHE_TESTNET]: AvalancheLogo,
+  [ChainId.HECO]: HecoLogo,
+  [ChainId.HECO_TESTNET]: HecoLogo,
+  [ChainId.HARMONY]: HarmonyLogo,
+  [ChainId.HARMONY_TESTNET]: HarmonyLogo,
+  [ChainId.OKEX]: OKExLogo,
+  [ChainId.OKEX_TESTNET]: OKExLogo,
+  [ChainId.ARBITRUM]: EthereumLogo,
+  [ChainId.ARBITRUM_TESTNET]: EthereumLogo,
+  [ChainId.CELO]: CeloLogo,
+  [ChainId.PALM]: PalmLogo,
+  [ChainId.PALM_TESTNET]: PalmLogo,
+  [ChainId.MOONRIVER]: MovrLogo,
+  [ChainId.FUSE]: FuseLogo,
+  [ChainId.TELOS]: TelosLogo,
+  [ChainId.HARDHAT]: EthereumLogo,
+  [ChainId.MOONBEAM]: MoonbeamLogo,
+  [ChainId.OPTIMISM]: EthereumLogo,
 }
