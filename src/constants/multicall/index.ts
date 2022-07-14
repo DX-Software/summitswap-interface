f5b47c3b78690f49660c027e69b5238ae81224eb
lukesamkharadze <lukesamkharadze@gmail.com>
2022-07-14 18:15:16 +0200

    feat(multi-call): add other networks

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -1,9 +1,30 @@
 import { ChainId } from '@koda-finance/summitswap-sdk'
 import MULTICALL_ABI from './abi.json'
 
-const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
-  [ChainId.BSCTESTNET]: `${process.env.REACT_APP_MULTI_CALL_TEST}`,
+const MULTICALL_NETWORKS = {
+  [ChainId.ETHEREUM]: '',
+  [ChainId.ROPSTEN]: '',
+  [ChainId.RINKEBY]: '',
+  [ChainId.GÖRLI]: '',
+  [ChainId.KOVAN]: '',
+  [ChainId.MATIC]: '',
+  [ChainId.MATIC_TESTNET]: '',
+  [ChainId.OPTIMISM]: '',
+  [ChainId.ARBITRUM]: '',
+  [ChainId.MOONBEAM]: '',
+  [ChainId.AVALANCHE]: '',
+  [ChainId.BSC]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb',
+  [ChainId.BSC_TESTNET]: '0x2D1a3Cbe60bAe408eC5cCCE0aEde0fD6B95fd7ec',
+  [ChainId.FANTOM]: '',
+  [ChainId.CELO]: '',
+  [ChainId.HARMONY]: '',
+  [ChainId.MOONRIVER]: '',
+  [ChainId.XDAI]: '',
+  [ChainId.TELOS]: '',
+  [ChainId.FUSE]: '',
+  [ChainId.OKEX]: '',
+  [ChainId.HECO]: '',
+  [ChainId.PALM]: '',
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
