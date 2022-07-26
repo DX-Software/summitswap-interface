c71e0df6a8ee9e5c592fff03bcdab6fb22e31c5a
erwin-huang <erwinhuank@gmail.com>
2022-07-26 11:46:24 +0700

    refactor: remove unused client

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,35 +3,12 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 
 const linkNodeGraph = process.env.REACT_APP_LINK_GRAPH_NODE
-const linkNodeGraphBlock = process.env.REACT_APP_LINK_GRAPH_NODE_BLOCK
 
+/* eslint-disable import/prefer-default-export */
 export const client = new ApolloClient({
   link: new HttpLink({
     uri: linkNodeGraph,
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
-})
-
-export const v1Client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap',
-  }),
-  cache: new InMemoryCache(),
-  shouldBatch: true,
-})
-
-export const stakingClient = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/way2rach/talisman',
-  }),
-  cache: new InMemoryCache(),
-  shouldBatch: true,
-})
-
-export const blockClient = new ApolloClient({
-  link: new HttpLink({
-    uri: linkNodeGraphBlock,
-  }),
-  cache: new InMemoryCache(),
 })
