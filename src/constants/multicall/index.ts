2db203d2776b7f2165d2b45cb41b4342cf72d9dc
sxio <stephen.huang96@gmail.com>
2022-07-28 17:40:53 +0700

    update multicall for tesnet

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,8 +2,8 @@ import { ChainId } from '@koda-finance/summitswap-sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B', // TODO
-  [ChainId.BSCTESTNET]: `${process.env.REACT_APP_MULTI_CALL_TEST}`,
+  [ChainId.MAINNET]: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B',
+  [ChainId.BSCTESTNET]: "0x9e84Ef65Bde2905426e3fEf5448a503eaa44491f",
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
