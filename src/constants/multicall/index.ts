14df1a4bfb1572409a6199c479d27e44ece1b3cf
erwin-huang <erwinhuank@gmail.com>
2022-07-28 21:30:38 +0700

    feat: update multicall contract address

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -3,7 +3,8 @@ import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B',
-  [ChainId.BSCTESTNET]: "0x9e84Ef65Bde2905426e3fEf5448a503eaa44491f",
+  [ChainId.BSCTESTNET]: '0x8F3273Fb89B075b1645095ABaC6ed17B2d4Bc576',
 }
+
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
