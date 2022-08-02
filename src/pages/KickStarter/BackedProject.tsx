848c744afd4e35a9a04871f0f57eaf3714cd4dcb
erwin-huang <erwinhuank@gmail.com>
2022-08-02 19:07:38 +0700

    feat: add BackProject UI

--- src/pages/KickStarter/BackedProject.tsx
+++ src/pages/KickStarter/BackedProject.tsx
@@ -1,8 +1,11 @@
 import { Button, Flex, Heading, useWalletModal, WalletIcon } from '@koda-finance/summitswap-uikit'
+import { Grid } from '@mui/material'
 import { useWeb3React } from '@web3-react/core'
 import React, { useState, useCallback } from 'react'
 import login from 'utils/login'
 import { TranslateString } from 'utils/translateTextHelpers'
+import ProjectCard from './ProjectCard'
+import ProjectDetails from './ProjectDetails'
 
 function BackedProject() {
   const { account, activate, deactivate } = useWeb3React()
@@ -15,19 +18,53 @@ function BackedProject() {
 
   const { onPresentConnectModal } = useWalletModal(handleLogin, deactivate, account as string)
 
+  const [selectedProject, setSelectedProject] = useState("")
+
+  const toggleSelectedProject = () => {
+    setSelectedProject("")
+  }
+
+  if (!account) {
+    return (
+      <Flex mb={3} flexDirection="column" alignItems="center" justifyContent="center" height={300}>
+        <Heading size='lg' color="primaryDark" style={{ fontWeight: 400 }} marginBottom={38} textAlign="center">
+          Please connect your wallet to view your projects
+        </Heading>
+        <Button variant='tertiary' startIcon={<WalletIcon />} style={{ fontFamily: 'Poppins' }} onClick={onPresentConnectModal}>
+          {TranslateString(292, 'CONNECT WALLET')}
+        </Button>
+      </Flex>
+    )
+  }
+
+  if (selectedProject !== "") {
+    return <ProjectDetails toggleSelectedProject={toggleSelectedProject} />
+  }
+
   return (
-    <>
-      {!account && (
-        <Flex mb={3} flexDirection="column" alignItems="center" justifyContent="center" height={300}>
-          <Heading size='lg' color="primaryDark" style={{ fontWeight: 400 }} marginBottom={38} textAlign="center">
-            Please connect your wallet to view your projects
-          </Heading>
-          <Button variant='tertiary' startIcon={<WalletIcon />} style={{ fontFamily: 'Poppins' }} onClick={onPresentConnectModal}>
-            {TranslateString(292, 'CONNECT WALLET')}
-          </Button>
-        </Flex>
-      )}
-    </>
+    <Flex flexDirection="column">
+      <Heading size="xl" marginBottom="24px">Backed Projects</Heading>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+      </Grid>
+    </Flex>
   )
 }
 
