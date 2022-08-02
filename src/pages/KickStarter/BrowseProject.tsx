d0c67d4b922eb2377f70512ffe3bc9d8b84704e6
erwin-huang <erwinhuank@gmail.com>
2022-08-02 18:28:10 +0700

    feat: add BrowseProject

--- src/pages/KickStarter/BrowseProject.tsx
+++ src/pages/KickStarter/BrowseProject.tsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import { Flex, Heading, Input, Select, SortIcon } from '@koda-finance/summitswap-uikit';
+import { Grid } from '@mui/material';
+import React, { useState } from 'react'
+import BrowseProjectDetails from './BrowseProjectDetails';
+import ProjectCard from './ProjectCard';
 
 function BrowseProject() {
-  return <div>BrowseProject</div>
+  const sortOptions = [
+    {
+      label: 'Sort by Default',
+      value: 'default',
+    },
+    {
+      label: 'Sort by Name',
+      value: 'name',
+    },
+  ]
+  const [selectedProject, setSelectedProject] = useState("")
+
+  const toggleSelectedProject = () => {
+    setSelectedProject("")
+  }
+
+  if (selectedProject !== "") {
+    return <BrowseProjectDetails toggleSelectedProject={toggleSelectedProject} />
+  }
+
+  return (
+    <Flex flexDirection="column">
+      <Heading size='xl' marginBottom="24px">Browse Project</Heading>
+      <Heading size='lg' marginBottom="24px">End Soon Project</Heading>
+      <Grid container spacing={2} marginBottom="60px">
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+      </Grid>
+      <Heading size='lg' marginBottom="24px">Browse All Projects</Heading>
+      <Flex style={{ columnGap: "12px" }} marginBottom="24px">
+        <Input
+          type="search"
+          placeholder="Search project by name or creator name"
+        />
+        <Select
+          startIcon={<SortIcon color="text" />}
+          options={sortOptions}
+          minWidth="230px"
+        />
+      </Flex>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+        <Grid item xs={12} sm={6} lg={4}>
+          <ProjectCard onClick={() => setSelectedProject("ID")} />
+        </Grid>
+      </Grid>
+    </Flex>
+  )
 }
 
 export default BrowseProject;
---
+++ src/pages/KickStarter/BrowseProjectDetails.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+type Props = {
+  toggleSelectedProject: () => void
+}
+
+function BrowseProjectDetails({ toggleSelectedProject }: Props) {
+  return (
+    <div>
+      Public Project Details
+    </div>
+  )
+}
+
+export default BrowseProjectDetails
