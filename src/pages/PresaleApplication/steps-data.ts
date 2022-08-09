a74ea4b3c9436752f7c0d2fc2c8ca5e9fa6d5fbb
umaismateen <umais.mateen@gmail.com>
2022-08-09 16:36:05 +0500

    feat: add steps data

---
+++ src/pages/PresaleApplication/steps-data.ts
@@ -0,0 +1,46 @@
+const steps = [
+  {
+    name: 'Step 1',
+    title: 'Token Verification',
+    subTitle:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in viverra ornare adipiscing adipiscing praesent.',
+      
+  },
+  {
+    name: 'Step 2',
+    title: 'Create Token Presale (1)',
+    subTitle:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in viverra ornare adipiscing adipiscing praesent.',
+
+  },
+  {
+    name: 'Step 3',
+    title: 'Create Token Presale (2)',
+    subTitle:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in viverra ornare adipiscing adipiscing praesent.',
+
+  },
+  {
+    name: 'Step 4',
+    title: 'Presale Date & Time',
+    subTitle:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in viverra ornare adipiscing adipiscing praesent.',
+
+  },
+  {
+    name: 'Step 5',
+    title: 'Additional Informations',
+    subTitle:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in viverra ornare adipiscing adipiscing praesent.',
+
+  },
+  {
+    name: 'Final Step',
+    title: 'Presale Summary',
+    subTitle:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in viverra ornare adipiscing adipiscing praesent.',
+
+  },
+]
+
+export default steps
