c941eefd6a3f65cfba81c97e35acf70eaf30c72c
erwin-huang <erwinhuank@gmail.com>
2022-08-10 14:22:38 +0700

    feat: add backed projects to kickstarter context

--- src/contexts/kickstarter.tsx
+++ src/contexts/kickstarter.tsx
@@ -1,21 +1,30 @@
 import { useWalletModal } from "@koda-finance/summitswap-uikit";
 import { useWeb3React } from "@web3-react/core";
+import useBackedKickstarter, { Contribution } from "hooks/useBackedKickstarter";
 import React, { createContext, useCallback, useContext, useState } from "react"
 import login from "utils/login";
 
 type KickstarterContextProps = {
   account: string | null | undefined
   onPresentConnectModal: () => void
+  backedProjects: Contribution[]
+  backedProjectAddress?: string,
+  handleBackedProjectChanged: (address?: string) => void
 };
 
 
 const KickstarterContext = createContext<KickstarterContextProps>({
   account: null,
   onPresentConnectModal: () => null,
+  backedProjects: [],
+  backedProjectAddress: undefined,
+  handleBackedProjectChanged: (newAddress?: string) => null
 });
 
 export function KickstarterProvider({ children }: { children: React.ReactNode }) {
   const { account, activate, deactivate } = useWeb3React()
+  const backedProjects = useBackedKickstarter(account)
+  const [backedProjectAddress, setBackedProjectAddress] = useState<string | undefined>()
 
   const handleLogin = useCallback(
     (connectorId: string) => {
@@ -26,12 +35,21 @@ export function KickstarterProvider({ children }: { children: React.ReactNode })
 
   const { onPresentConnectModal } = useWalletModal(handleLogin, deactivate, account as string)
 
+  const handleBackedProjectChanged = (newAddress?: string) => {
+    setBackedProjectAddress(newAddress)
+  }
+
   return (
     <KickstarterContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         account,
-        onPresentConnectModal
+        onPresentConnectModal,
+
+        backedProjects,
+        backedProjectAddress,
+        handleBackedProjectChanged,
+
       }}
     >
       {children}
