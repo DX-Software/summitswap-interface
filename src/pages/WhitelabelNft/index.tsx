79f977aec9fe88f797ecd6c2f04d8ae7a56d9cc5
sxio <stephen.huang96@gmail.com>
2022-08-18 12:12:29 +0700

    feat: whitelabel index connect wallet button

--- src/pages/WhitelabelNft/index.tsx
+++ src/pages/WhitelabelNft/index.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { Button, Flex, useWalletModal } from '@koda-finance/summitswap-uikit'
+import { useWeb3React } from '@web3-react/core'
+import React, { useCallback } from 'react'
+import login from 'utils/login'
+import { TranslateString } from 'utils/translateTextHelpers'
 import CreateWhitelabelNftForm from './CreateForm'
 
 export default function WhitelabelNft() {
+  const { account, activate, deactivate } = useWeb3React()
+
+  const handleLogin = useCallback(
+    (connectorId: string) => {
+      login(connectorId, activate)
+    },
+    [activate]
+  )
+
+  const { onPresentConnectModal } = useWalletModal(handleLogin, deactivate, account as string)
+
+  if (!account) {
+    return (
+      <>
+        <Flex mb={3} mt={40} justifyContent="center">
+          <Button style={{ fontFamily: 'Poppins' }} onClick={onPresentConnectModal}>
+            {TranslateString(292, 'CONNECT WALLET')}
+          </Button>
+        </Flex>
+      </>
+    )
+  }
+
   return (
     <div className="main-content">
       <CreateWhitelabelNftForm />
