62623d170c33e3a2fe757d3b1661a3e54df42e41
sxio <stephen.huang96@gmail.com>
2022-08-18 12:12:07 +0700

    feat: whitelabel constant add phase enum

--- src/constants/whitelabel.ts
+++ src/constants/whitelabel.ts
@@ -1,2 +1,7 @@
-// eslint-disable-next-line import/prefer-default-export
 export const WHITELABEL_FACTORY_ADDRESS = `${process.env.REACT_APP_WHITELABEL_FACTORY_ADDRESS}`
+
+export enum Phase {
+  Pause,
+  Whitelist,
+  Public,
+}
