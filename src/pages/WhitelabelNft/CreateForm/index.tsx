bbf97ad803fe97c45c56cb772d6114b73067f20a
sxio <stephen.huang96@gmail.com>
2022-08-24 10:49:42 +0700

    fix: remove spreadsheet and web3 storage logic

--- src/pages/WhitelabelNft/CreateForm/index.tsx
+++ src/pages/WhitelabelNft/CreateForm/index.tsx
@@ -1,58 +1,41 @@
-import { Button, Flex, Image, Input, Text } from '@koda-finance/summitswap-uikit'
-import { Grid } from '@mui/material'
+import { Button, Flex, Input, Text } from '@koda-finance/summitswap-uikit'
 import { Phase } from 'constants/whitelabel'
 import { Field, FormikProps, FormikProvider, useFormik } from 'formik'
 import { useWhitelabelFactoryContract } from 'hooks/useContract'
 import React, { useCallback, useState } from 'react'
-import { convertFileToBase64 } from 'utils/convertFileToBase64'
-import parseMetadata from '../spreadsheet'
 import DragAndDrop from './DragAndDrop'
 
-const NftCardPreview = ({ nftMetadata }: { nftMetadata: NftMetadata }) => {
-  return (
-    <Flex>
-      <Image src={nftMetadata.image} width={100} height={100} />
-      <Flex flexDirection="column">
-        <Text>Name: {nftMetadata.name}</Text>
-        <Text>Description: {nftMetadata.description}</Text>
-      </Flex>
-    </Flex>
-  )
-}
+// const NftCardPreview = ({ nftMetadata }: { nftMetadata: NftMetadata }) => {
+//   return (
+//     <Flex>
+//       <Image src={nftMetadata.image} width={100} height={100} />
+//       <Flex flexDirection="column">
+//         <Text>Name: {nftMetadata.name}</Text>
+//         <Text>Description: {nftMetadata.description}</Text>
+//       </Flex>
+//     </Flex>
+//   )
+// }
 
 export default function CreateWhitelabelNftForm() {
-  const [nftImages, setNftImages] = useState<NftImage[]>([])
-  const [spreadsheet, setSpreadsheet] = useState<ArrayBuffer>()
-  const [nftMetadata, setNftMetadata] = useState<NftMetadata[]>([])
+  const [nftImages, setNftImages] = useState<File[]>([])
+  const [spreadsheet, setSpreadsheet] = useState<File>()
 
   const whitelabelFactoryContract = useWhitelabelFactoryContract()
 
   const handleImageOnChange = useCallback(async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       const files = Array.from(e.target.files)
-      const base64images = await Promise.all(files.map((file) => convertFileToBase64(file)))
-      const nftImagesTemp = files.map((file, index) => ({
-        id: Number(file.name.split('.')[0]),
-        base64: String(base64images[index]),
-      }))
-      setNftImages(nftImagesTemp)
+      setNftImages(files)
     }
   }, [])
 
   const handleSpreadsheetOnChange = useCallback(async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       const file = e.target.files[0]
-      const data = await file.arrayBuffer()
-      setSpreadsheet(data)
+      setSpreadsheet(file)
     }
   }, [])
-
-  const handleApply = useCallback(async () => {
-    if (spreadsheet) {
-      const parsedMetadata = parseMetadata(spreadsheet, nftImages)
-      setNftMetadata(parsedMetadata)
-    }
-  }, [spreadsheet, nftImages])
 
   const formik: FormikProps<WhitelabelFormValues> = useFormik<WhitelabelFormValues>({
     initialValues: {
@@ -81,82 +64,71 @@ export default function CreateWhitelabelNftForm() {
   return (
     <div className="main-content">
       <FormikProvider value={formik}>
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <Flex flexDirection="column">
-              <Text>Name</Text>
-              <Input
-                name="name"
-                placeholder="Name"
-                onChange={formik.handleChange}
-                style={{ marginBottom: 12 }}
-                as={Field}
-              />
-              <Text>Symbol</Text>
-              <Input
-                name="symbol"
-                placeholder="Symbol"
-                onChange={formik.handleChange}
-                style={{ marginBottom: 12 }}
-                as={Field}
-              />
-              <Text>Max Supply</Text>
-              <Input
-                name="maxSupply"
-                placeholder="Max Supply"
-                onChange={formik.handleChange}
-                style={{ marginBottom: 12 }}
-                as={Field}
-              />
-              <Text>Whitelist Mint Price</Text>
-              <Input
-                name="whitelistMintPrice"
-                placeholder="Whitelist Mint Price"
-                onChange={formik.handleChange}
-                style={{ marginBottom: 12 }}
-                as={Field}
-              />
-              <Text>Public Mint Price</Text>
-              <Input
-                name="publicMintPrice"
-                placeholder="Public Mint Price"
-                onChange={formik.handleChange}
-                style={{ marginBottom: 12 }}
-                as={Field}
-              />
-              <Text>Signer</Text>
-              <Input
-                name="signer"
-                placeholder="Signer Address"
-                onChange={formik.handleChange}
-                style={{ marginBottom: 12 }}
-                as={Field}
-              />
-              <Flex justifyContent="space-between" style={{ marginBottom: 12 }}>
-                <DragAndDrop name="images" accept="image/*" multiple handleChange={handleImageOnChange}>
-                  {nftImages.length > 0 ? `${nftImages.length} images selected` : 'Drag and Drop your NFT images here'}
-                </DragAndDrop>
-                <DragAndDrop
-                  name="spreadsheet"
-                  accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                  handleChange={handleSpreadsheetOnChange}
-                >
-                  {spreadsheet ? 'Spreadsheet selected' : 'Upload your metadata spreadsheet here'}
-                </DragAndDrop>
-              </Flex>
-              <Button marginX="auto" onClick={handleApply}>
-                Apply
-              </Button>
-              <Button onClick={formik.submitForm}>Submit</Button>
-            </Flex>
-          </Grid>
-          <Grid item xs={4}>
-            <Text>NFT Preview</Text>
-            {nftMetadata.map((nftMetadatum) => (
-              <NftCardPreview nftMetadata={nftMetadatum} />
-            ))}
-          </Grid>
-        </Grid>
+        <Flex flexDirection="column">
+          <Text>Name</Text>
+          <Input
+            name="name"
+            placeholder="Name"
+            onChange={formik.handleChange}
+            style={{ marginBottom: 12 }}
+            as={Field}
+          />
+          <Text>Symbol</Text>
+          <Input
+            name="symbol"
+            placeholder="Symbol"
+            onChange={formik.handleChange}
+            style={{ marginBottom: 12 }}
+            as={Field}
+          />
+          <Text>Max Supply</Text>
+          <Input
+            name="maxSupply"
+            placeholder="Max Supply"
+            onChange={formik.handleChange}
+            style={{ marginBottom: 12 }}
+            as={Field}
+          />
+          <Text>Whitelist Mint Price</Text>
+          <Input
+            name="whitelistMintPrice"
+            placeholder="Whitelist Mint Price"
+            onChange={formik.handleChange}
+            style={{ marginBottom: 12 }}
+            as={Field}
+          />
+          <Text>Public Mint Price</Text>
+          <Input
+            name="publicMintPrice"
+            placeholder="Public Mint Price"
+            onChange={formik.handleChange}
+            style={{ marginBottom: 12 }}
+            as={Field}
+          />
+          <Text>Signer</Text>
+          <Input
+            name="signer"
+            placeholder="Signer Address"
+            onChange={formik.handleChange}
+            style={{ marginBottom: 12 }}
+            as={Field}
+          />
+          <Flex justifyContent="space-around" style={{ marginBottom: 12 }}>
+            <DragAndDrop name="images" accept="image/*" multiple handleChange={handleImageOnChange}>
+              {nftImages.length > 0 ? `${nftImages.length} images selected` : 'Drag and Drop your NFT images here'}
+            </DragAndDrop>
+            <DragAndDrop
+              name="spreadsheet"
+              accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+              handleChange={handleSpreadsheetOnChange}
+            >
+              {spreadsheet ? 'Spreadsheet selected' : 'Upload your metadata spreadsheet here'}
+            </DragAndDrop>
+          </Flex>
+          <Button marginX="auto" onClick={formik.submitForm}>
+            Submit
+          </Button>
+        </Flex>
       </FormikProvider>
     </div>
   )
--- src/pages/WhitelabelNft/types.d.ts
+++ src/pages/WhitelabelNft/types.d.ts
@@ -7,38 +7,3 @@ interface WhitelabelFormValues {
   signer: string
   phase: number
 }
-
-type NftImage = {
-  id: number
-  base64: string
-}
-
-type TotalNftSheet = {
-  totalNft: number
-}
-
-type NftTraitSheet = {
-  displayType: string
-  traitType: string
-}
-
-type NftMetadataSheet = {
-  tokenId: number
-  name: string
-  description: string
-  [key: string]: string
-}
-
-type NftAttribute = {
-  trait_type?: string
-  display_type?: DisplayType
-  value: string | number
-}
-
-type NftMetadata = JSON & {
-  name: string
-  description: string
-  external_url?: string
-  image: string
-  attributes: NftAttribute[]
-}
