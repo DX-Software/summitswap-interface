fe8bb9962dca79d328944dd09c89886576fd1f9d
sxio <stephen.huang96@gmail.com>
2022-08-24 13:51:47 +0700

    feat(useWhitelabelNft): useWhitelabelNftUpload

---
+++ src/api/useWhitelabelNft.ts
@@ -0,0 +1,20 @@
+import { useMutation } from 'react-query'
+import httpClient from './http'
+
+const URL = 'whitelabel-nft'
+
+// eslint-disable-next-line import/prefer-default-export
+export function useWhitelabelNftUpload() {
+  return useMutation(async (data: WhitelabelUploadParameter) => {
+    const formData = new FormData()
+    formData.append('walletAddress', data.walletAddress)
+    formData.append('spreadsheet', data.spreadsheet)
+    data.nftImages.forEach((nftImage) => {
+      formData.append('images', nftImage)
+    })
+
+    const res = await httpClient.post(`${URL}/upload`, formData)
+
+    return res.data as WhitelabelUploadResult
+  })
+}
