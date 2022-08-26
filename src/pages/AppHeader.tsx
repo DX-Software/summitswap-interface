c250f1af519f5bf99687209251ce46bb754b2e65
sxio <stephen.huang96@gmail.com>
2022-08-26 09:44:01 +0700

    feat(AppHeader): add header whitelabel Nft

--- src/pages/AppHeader.tsx
+++ src/pages/AppHeader.tsx
@@ -57,6 +57,7 @@ export default function AppHeader() {
         {location.pathname.search('/staking') !== -1 && 'Staking'}
         {location.pathname === '/create-token' && 'Create Token'}
         {location.pathname === '/presale' && 'Create Presale'}
+        {location.pathname === '/whitelabel-nft' && 'Whitelabel NFT'}
         {location.pathname.search('/info') !== -1 && 'SummitSwap Info & Analytics'}
       </StyledBox>
       <TopLogo src={TopLogoIcon} alt="" />
