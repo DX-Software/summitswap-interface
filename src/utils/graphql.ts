d22f867f518027d8e14d989d8206fe1c1ff7d020
sxio <stephen.huang96@gmail.com>
2022-08-26 12:48:57 +0700

    feat: whitelabe nft graphql

--- src/utils/graphql.ts
+++ src/utils/graphql.ts
@@ -1,5 +1,7 @@
-import { INFO_CLIENT } from 'constants/graphs'
+import { INFO_CLIENT, WHITELABEL_NFT_CLIENT } from 'constants/graphs'
 import { GraphQLClient } from 'graphql-request'
 
 /* eslint-disable import/prefer-default-export */
 export const infoClient = new GraphQLClient(INFO_CLIENT)
+
+export const whitelabelNftClient = new GraphQLClient(WHITELABEL_NFT_CLIENT)
