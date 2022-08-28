67f74954bec26c46c2b3259acfd5d66e5104ccea
umaismateen <umais.mateen@gmail.com>
2022-08-29 03:55:41 +0500

    refactor: wallet connection

--- src/pages/PresaleApplication/index.tsx
+++ src/pages/PresaleApplication/index.tsx
@@ -2,16 +2,25 @@
 import React, { useState, useCallback, useEffect } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { TranslateString } from 'utils/translateTextHelpers'
-import { Button, useWalletModal, Box, ButtonMenu, ButtonMenuItem } from '@koda-finance/summitswap-uikit'
+import {
+  Box,
+  Text,
+  Button,
+  WalletIcon,
+  ButtonMenu,
+  useWalletModal,
+  ButtonMenuItem,
+} from '@koda-finance/summitswap-uikit'
 import { useFactoryPresaleContract } from 'hooks/useContract'
 import { PRESALE_FACTORY_ADDRESS } from 'constants/presale'
 import login from 'utils/login'
 import CreatePresale from './CreatePresale'
 import AdminPanel from './AdminPanel'
+import MyPresales from './MyPresales'
 
 export default function PresaleApplication() {
   const { account, activate, deactivate } = useWeb3React()
-  const [buttonIndex, setButtonIndex] = useState(0)
+  const [buttonIndex, setButtonIndex] = useState(1)
   const [accountIsAdmin, setAccountIsAdmin] = useState(false)
 
   const factoryContract = useFactoryPresaleContract(PRESALE_FACTORY_ADDRESS)
@@ -48,10 +57,22 @@ export default function PresaleApplication() {
       </Box>
       {accountIsAdmin && buttonIndex === 0 && <AdminPanel />}
       {buttonIndex === 1 && <CreatePresale />}
+      {buttonIndex === 2 && <MyPresales setHomeButtonIndex={setButtonIndex} />}
     </>
   ) : (
-    <Button m={40} style={{ fontFamily: 'Poppins' }} onClick={onPresentConnectModal}>
-      {TranslateString(292, 'CONNECT WALLET')}
-    </Button>
+    <>
+      <Text marginTop="100px" fontSize="20px" color="primaryDark">
+        Please connect your wallet
+      </Text>
+      <Button
+        startIcon={<WalletIcon />}
+        variant="tertiary"
+        m={40}
+        style={{ fontFamily: 'Poppins' }}
+        onClick={onPresentConnectModal}
+      >
+        {TranslateString(292, 'CONNECT WALLET')}
+      </Button>
+    </>
   )
 }
