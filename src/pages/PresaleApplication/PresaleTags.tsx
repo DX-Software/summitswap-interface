62ae523c1e199a914cb2d671accb5adcd2389b0a
umaismateen <umais.mateen@gmail.com>
2022-08-29 03:53:09 +0500

    feat: update presaleTags component

--- src/pages/PresaleApplication/PresaleTags.tsx
+++ src/pages/PresaleApplication/PresaleTags.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, Tag, Text, LockIcon, UnLockIcon, CircleFilledIcon } from '@koda-finance/summitswap-uikit'
 import styled from 'styled-components'
 import { RowFixed } from 'components/Row'
-import { PresaleInfo } from './types'
+import { checkSalePhase } from 'utils/presale'
+import { PresaleInfo, PresalePhases } from './types'
 
 const StyledText = styled(Text)`
   font-size: ${({ fontSize }) => fontSize || '16px'};
@@ -11,24 +12,54 @@ const StyledText = styled(Text)`
   }
 `
 
-const PresaleTags = ({ presaleInfo }: { presaleInfo: PresaleInfo | undefined }) => {
+interface Props {
+  isAccountWhitelisted?: boolean
+  presaleInfo?: PresaleInfo
+}
+
+const PresaleTags = ({ presaleInfo, isAccountWhitelisted }: Props) => {
+  const [presalePhase, setPresalePhase] = useState('')
+  const [currentTime, setCurrentTime] = useState(new Date())
+
+  useEffect(() => {
+    if (presaleInfo) {
+      const presalePhase_ = checkSalePhase(presaleInfo)
+      const timer = setTimeout(() => {
+        if (presalePhase_ !== presalePhase) setPresalePhase(presalePhase_)
+        if (presalePhase === PresalePhases.PresalePhase || presalePhase === PresalePhases.PresaleNotStarted) {
+          setCurrentTime(new Date())
+        }
+      }, 1000)
+      return () => clearTimeout(timer)
+    }
+    return undefined
+  }, [presaleInfo, currentTime, presalePhase])
+
+  useEffect(() => {
+    if (presaleInfo) {
+      setPresalePhase(checkSalePhase(presaleInfo))
+    }
+  }, [presaleInfo])
+
   const SaleTypeTage = () => (
     <Box marginLeft="6px">
-      {presaleInfo?.isVestingEnabled ? (
-        <Tag startIcon={<LockIcon width="10px" color="textDisabled" />} variant="default">
-          <StyledText color="black" bold fontSize="12px">
-            WHITELIST ONLY
-          </StyledText>
-        </Tag>
-      ) : (
-        <Tag startIcon={<UnLockIcon width="10px" color="primary" />} variant="default">
-          <StyledText color="primary" bold fontSize="12px">
-            PUBLIC
-          </StyledText>
-        </Tag>
-      )}
+      {presaleInfo?.isWhitelistEnabled &&
+        (isAccountWhitelisted ? (
+          <Tag startIcon={<UnLockIcon width="10px" color="primary" />} variant="default">
+            <StyledText color="primary" bold fontSize="12px">
+              WHITELIST ONLY
+            </StyledText>
+          </Tag>
+        ) : (
+          <Tag startIcon={<LockIcon width="10px" color="textDisabled" />} variant="default">
+            <StyledText color="black" bold fontSize="12px">
+              WHITELIST ONLY
+            </StyledText>
+          </Tag>
+        ))}
     </Box>
   )
+
   if (!presaleInfo) return <Box height="26px" />
 
   if (!presaleInfo.isApproved)
@@ -39,7 +70,7 @@ const PresaleTags = ({ presaleInfo }: { presaleInfo: PresaleInfo | undefined }) 
         </StyledText>
       </Tag>
     )
-  if (new Date() < new Date(presaleInfo.startPresaleTime.mul(1000).toNumber())) {
+  if (presalePhase === PresalePhases.PresaleNotStarted) {
     return (
       <RowFixed>
         <Tag variant="binance">
@@ -51,7 +82,7 @@ const PresaleTags = ({ presaleInfo }: { presaleInfo: PresaleInfo | undefined }) 
       </RowFixed>
     )
   }
-  if (new Date() < new Date(presaleInfo.endPresaleTime.mul(1000).toNumber())) {
+  if (presalePhase === PresalePhases.PresalePhase) {
     return (
       <RowFixed>
         <Tag startIcon={<CircleFilledIcon color="currentColor" width="10px" />} variant="failure">
@@ -63,12 +94,24 @@ const PresaleTags = ({ presaleInfo }: { presaleInfo: PresaleInfo | undefined }) 
       </RowFixed>
     )
   }
-  if (new Date() > new Date(presaleInfo.endPresaleTime.mul(1000).toNumber())) {
+  if (presalePhase === PresalePhases.PresaleEnded || presalePhase === PresalePhases.ClaimPhase) {
     return (
       <RowFixed>
         <Tag bold variant="textDisabled">
           <StyledText bold fontSize="12px">
             ENDED
+          </StyledText>
+        </Tag>
+        {SaleTypeTage()}
+      </RowFixed>
+    )
+  }
+  if (presalePhase === PresalePhases.PresaleCancelled) {
+    return (
+      <RowFixed>
+        <Tag bold variant="textDisabled">
+          <StyledText bold fontSize="12px">
+            CANCELLED
           </StyledText>
         </Tag>
         {SaleTypeTage()}
