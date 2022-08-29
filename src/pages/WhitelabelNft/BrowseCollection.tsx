c7389f690ccdbff66478573d946e71cd256700da
sxio <stephen.huang96@gmail.com>
2022-08-29 13:47:43 +0700

    feat: browse collection

--- src/pages/WhitelabelNft/BrowseCollection.tsx
+++ src/pages/WhitelabelNft/BrowseCollection.tsx
@@ -1,34 +1,40 @@
-import { Input, TabPresale, Text } from '@koda-finance/summitswap-uikit'
-import { useWhitelabelNftContext } from 'contexts/whitelabelNft'
+import { Box, Input, TabPresale, Text } from '@koda-finance/summitswap-uikit'
+import { Phase } from 'constants/whitelabel'
+import useWhitelabelNfts from 'hooks/useWhitelabelNfts'
+import useWhitelabelNftsByPhase from 'hooks/useWhitelabelNftsByPhase'
 import React, { useState } from 'react'
 import InfoSection from './InfoSection'
+import NftCollectionGallery from './shared/NftCollectionGallery'
 import { CollectionTab, NavItem } from './types'
 
 function BrowseCollection() {
   const [tabActiveIndex, setTabActiveIndex] = useState(0)
 
-  const { whitelabelNfts } = useWhitelabelNftContext()
+  const whitelabelNfts = useWhitelabelNfts()
+  const publicPhaseWhitelabelNfts = useWhitelabelNftsByPhase(Phase.Public)
+  const whitelistPhaseWhitelabelNfts = useWhitelabelNftsByPhase(Phase.Whitelist)
+  const pausedPhaseWhitelabelNfts = useWhitelabelNftsByPhase(Phase.Pause)
 
   const collectionTabs: NavItem[] = [
     {
       label: 'All Collections',
       code: CollectionTab.ALL_COLLECTION,
-      component: null,
+      component: <NftCollectionGallery queryResult={whitelabelNfts} />,
     },
     {
       label: 'Public Phase',
       code: CollectionTab.PUBLIC_PHASE,
-      component: null,
+      component: <NftCollectionGallery queryResult={publicPhaseWhitelabelNfts} />,
     },
     {
       label: 'Whitelist Phase',
       code: CollectionTab.WHITELIST_PHASE,
-      component: null,
+      component: <NftCollectionGallery queryResult={whitelistPhaseWhitelabelNfts} />,
     },
     {
       label: 'Paused Phase',
       code: CollectionTab.PAUSED_PHASE,
-      component: null,
+      component: <NftCollectionGallery queryResult={pausedPhaseWhitelabelNfts} />,
     },
   ]
 
@@ -36,12 +42,14 @@ function BrowseCollection() {
     <>
       <Input placeholder="Seach NFT Collection by name" scale="lg" style={{ marginBottom: '32px' }} />
       <InfoSection />
-      <TabPresale activeIndex={tabActiveIndex} onItemClick={(index: number) => setTabActiveIndex(index)}>
-        {collectionTabs.map((item) => {
-          return <Text>{item.label}</Text>
-        })}
-      </TabPresale>
-      <div className="main-content">{collectionTabs[tabActiveIndex].component}</div>
+      <Box marginBottom="24px">
+        <TabPresale activeIndex={tabActiveIndex} onItemClick={(index: number) => setTabActiveIndex(index)}>
+          {collectionTabs.map((item) => {
+            return <Text>{item.label}</Text>
+          })}
+        </TabPresale>
+      </Box>
+      <div>{collectionTabs[tabActiveIndex].component}</div>
     </>
   )
 }
