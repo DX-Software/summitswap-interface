4c6d124310b85837d419bd97b57ced673beb1a76
umaismateen <umais.mateen@gmail.com>
2022-09-01 14:10:51 +0500

    fix(CreationStep03): choose label to choose radio value

--- src/pages/PresaleApplication/CreatePresale/CreationStep03.tsx
+++ src/pages/PresaleApplication/CreatePresale/CreationStep03.tsx
@@ -64,44 +64,50 @@ const CreationStep03 = ({ currency, selectedToken, formik, changeStepNumber }: P
               <RowFixed marginBottom="8px">
                 <Box>
                   <Radio
+                    id={`${FieldNames.refundType}-${RADIO_VALUES.REFUND_TYPE_REFUND}`}
                     scale="sm"
                     name={FieldNames.refundType}
                     value={RADIO_VALUES.REFUND_TYPE_REFUND}
                     checked={Number(formik.values.refundType) === RADIO_VALUES.REFUND_TYPE_REFUND}
                   />
                 </Box>
-                <label htmlFor="refund">
-                  <Text
-                    color={Number(formik.values.refundType) === RADIO_VALUES.REFUND_TYPE_REFUND ? 'linkColor' : ''}
-                    marginLeft="8px"
-                  >
-                    Refund
-                  </Text>
+                <Box>
+                  <label htmlFor={`${FieldNames.refundType}-${RADIO_VALUES.REFUND_TYPE_REFUND}`}>
+                    <Text
+                      color={Number(formik.values.refundType) === RADIO_VALUES.REFUND_TYPE_REFUND ? 'linkColor' : ''}
+                      marginLeft="8px"
+                    >
+                      Refund
+                    </Text>
+                  </label>
                   <Caption marginX="8px" color="textDisabled">
                     Refund remaining presale Token after finalizing
                   </Caption>
-                </label>
+                </Box>
               </RowFixed>
               <RowFixed>
                 <Box>
                   <Radio
+                    id={`${FieldNames.refundType}-${RADIO_VALUES.REFUND_TYPE_BURN}`}
                     scale="sm"
                     name={FieldNames.refundType}
                     value={RADIO_VALUES.REFUND_TYPE_BURN}
                     checked={Number(formik.values.refundType) === RADIO_VALUES.REFUND_TYPE_BURN}
                   />
                 </Box>
-                <label htmlFor="burn">
-                  <Text
-                    marginLeft="8px"
-                    color={Number(formik.values.refundType) === RADIO_VALUES.REFUND_TYPE_BURN ? 'linkColor' : ''}
-                  >
-                    Burn
-                  </Text>
+                <Box>
+                  <label htmlFor={`${FieldNames.refundType}-${RADIO_VALUES.REFUND_TYPE_BURN}`}>
+                    <Text
+                      marginLeft="8px"
+                      color={Number(formik.values.refundType) === RADIO_VALUES.REFUND_TYPE_BURN ? 'linkColor' : ''}
+                    >
+                      Burn
+                    </Text>
+                  </label>
                   <Caption marginX="8px" color="textDisabled">
                     Burn remaining presale Token after finalizing
                   </Caption>
-                </label>
+                </Box>
               </RowFixed>
             </Box>
           </Box>
@@ -122,12 +128,13 @@ const CreationStep03 = ({ currency, selectedToken, formik, changeStepNumber }: P
                 <Box>
                   <Radio
                     scale="sm"
+                    id={`${FieldNames.listingChoice}-${RADIO_VALUES.LISTING_SS_100}`}
                     name={FieldNames.listingChoice}
                     value={RADIO_VALUES.LISTING_SS_100}
                     checked={Number(formik.values.listingChoice) === RADIO_VALUES.LISTING_SS_100}
                   />
                 </Box>
-                <label htmlFor="summitswap">
+                <label htmlFor={`${FieldNames.listingChoice}-${RADIO_VALUES.LISTING_SS_100}`}>
                   <Text
                     color={Number(formik.values.listingChoice) === RADIO_VALUES.LISTING_SS_100 ? 'linkColor' : ''}
                     marginLeft="8px"
@@ -140,12 +147,13 @@ const CreationStep03 = ({ currency, selectedToken, formik, changeStepNumber }: P
                 <Box>
                   <Radio
                     scale="sm"
+                    id={`${FieldNames.listingChoice}-${RADIO_VALUES.LISTING_PS_100}`}
                     name={FieldNames.listingChoice}
                     value={RADIO_VALUES.LISTING_PS_100}
                     checked={Number(formik.values.listingChoice) === RADIO_VALUES.LISTING_PS_100}
                   />
                 </Box>
-                <label htmlFor="pancakeswap">
+                <label htmlFor={`${FieldNames.listingChoice}-${RADIO_VALUES.LISTING_PS_100}`}>
                   <Text
                     marginLeft="8px"
                     color={Number(formik.values.listingChoice) === RADIO_VALUES.LISTING_PS_100 ? 'linkColor' : ''}
@@ -158,22 +166,25 @@ const CreationStep03 = ({ currency, selectedToken, formik, changeStepNumber }: P
                 <Box>
                   <Radio
                     scale="sm"
+                    id={`${FieldNames.listingChoice}-${RADIO_VALUES.LISTING_SS75_PK25}`}
                     name={FieldNames.listingChoice}
                     value={RADIO_VALUES.LISTING_SS75_PK25}
                     checked={Number(formik.values.listingChoice) === RADIO_VALUES.LISTING_SS75_PK25}
                   />
                 </Box>
-                <label htmlFor="both">
-                  <Text
-                    marginLeft="8px"
-                    color={Number(formik.values.listingChoice) === RADIO_VALUES.LISTING_SS75_PK25 ? 'linkColor' : ''}
-                  >
-                    Both
-                  </Text>
+                <Box>
+                  <label htmlFor={`${FieldNames.listingChoice}-${RADIO_VALUES.LISTING_SS75_PK25}`}>
+                    <Text
+                      marginLeft="8px"
+                      color={Number(formik.values.listingChoice) === RADIO_VALUES.LISTING_SS75_PK25 ? 'linkColor' : ''}
+                    >
+                      Both
+                    </Text>
+                  </label>
                   <Caption marginLeft="8px" color="textDisabled">
                     This will be listed to 75% SS and 25% PS
                   </Caption>
-                </label>
+                </Box>
               </RowFixed>
             </Box>
           </Box>
@@ -211,7 +222,7 @@ const CreationStep03 = ({ currency, selectedToken, formik, changeStepNumber }: P
               <Caption color={formik.touched.liquidity && !!formik.errors.liquidity ? 'failure' : 'textDisabled'}>
                 {formik.touched.liquidity && formik.errors.liquidity
                   ? formik.errors.liquidity
-                  : 'Enter the percentage of raised funds that should be allocated to Liquidity on Pancakeswap (Min 25%, Max 100%)'}
+                  : 'Enter the percentage of raised funds that should be allocated to Liquidity on Summitswap (Min 25%, Max 100%)'}
               </Caption>
             </StyledInputWrapper>
             <StyledInputWrapper>
@@ -264,13 +275,14 @@ const CreationStep03 = ({ currency, selectedToken, formik, changeStepNumber }: P
                   <RowFixed marginBottom="4px" marginRight="4px" key={key}>
                     <Box>
                       <Radio
+                        id={`${FieldNames.listingToken}-${key}`}
                         scale="sm"
                         name={FieldNames.listingToken}
                         value={TOKEN_CHOICES[key]}
                         checked={formik.values.listingToken === TOKEN_CHOICES[key]}
                       />
                     </Box>
-                    <label htmlFor={key}>
+                    <label htmlFor={`${FieldNames.listingToken}-${key}`}>
                       <Text
                         color={formik.values.listingToken === TOKEN_CHOICES[key] ? 'linkColor' : ''}
                         marginLeft="8px"
