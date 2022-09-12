45320187bbf22a37c318dcb8a97cc8dbe666296f
sxio <stephen.huang96@gmail.com>
2022-09-12 18:44:16 +0700

    fix(whitelabel nft): info section

--- src/pages/WhitelabelNft/BrowseCollections/InfoSection.tsx
+++ src/pages/WhitelabelNft/BrowseCollections/InfoSection.tsx
@@ -1,11 +1,16 @@
 import { CloseIcon, Flex, Text } from '@koda-finance/summitswap-uikit'
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
+import { isMobile } from 'react-device-detect'
 import styled from 'styled-components'
 
 const Wrapper = styled(Flex)`
+  position: relative;
   background-color: ${({ theme }) => theme.colors.inputColor};
   border-radius: 16px;
   overflow: hidden;
+
+  @media (max-width: 576px) {
+  }
 `
 
 const InfoImage = styled.img`
@@ -14,10 +19,69 @@ const InfoImage = styled.img`
   rotate: 15deg;
   left: -32px;
   width: auto;
+
+  @media (max-width: 576px) {
+    width: 140px;
+    position: absolute;
+    opacity: 0.2;
+    left: -24px;
+  }
+`
+
+const InfoWrapper = styled(Flex)`
+  flex-direction: column;
+  padding: 24px;
+  padding-left: 0;
+
+  @media (max-width: 576px) {
+    padding: 12px 16px;
+  }
+`
+
+const Info = styled(Text)`
+  font-size: 14px;
+  display: inline-block;
+
+  @media (max-width: 576px) {
+    font-size: 12px;
+  }
 `
 
 function InfoSection() {
   const [hide, setHide] = useState(false)
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const infoRaw = `Ac in bibendum lectus eget maecenas quis dolor, sociis dignissim. Viverra a elementum vitae elementum, porta
+  gravida elit eu. Tincidunt facilisis integer urna, quam faucibus faucibus. Neque, cursus nibh vitae
+  vestibulum, pharetra justo, at venenatis euismod. Massa at adipiscing egestas auctor. Ac massa nisl nibh
+  egestas sed.`
+
+  const infoTruncated = useMemo(() => {
+    if (infoRaw.length > 160) {
+      return `${infoRaw.slice(0, 161)}...`
+    }
+    return infoRaw
+  }, [infoRaw])
+
+  const isTruncated = useMemo(() => {
+    if (isMobile && !isExpanded) {
+      return true
+    }
+    return false
+  }, [isExpanded])
+
+  const info = useMemo(() => {
+    if (isTruncated) {
+      return infoTruncated
+    }
+    return infoRaw
+  }, [isTruncated, infoTruncated, infoRaw])
+
+  useEffect(() => {
+    if (!isMobile) {
+      setIsExpanded(true)
+    }
+  }, [])
 
   if (hide) {
     return null
@@ -26,20 +90,22 @@ function InfoSection() {
   return (
     <Wrapper marginBottom="32px">
       <InfoImage src="/images/koda-mascot.png" />
-      <Flex flexDirection="column" padding={24} paddingLeft={0}>
+      <InfoWrapper>
         <Flex justifyContent="between" marginBottom="8px">
           <Text bold color="linkColor">
             What is Whitelabel NFT?
           </Text>
           <CloseIcon color="failure" marginLeft="auto" onClick={() => setHide(true)} cursor="pointer" width="24px" />
         </Flex>
-        <Text fontSize="14px">
-          Ac in bibendum lectus eget maecenas quis dolor, sociis dignissim. Viverra a elementum vitae elementum, porta
-          gravida elit eu. Tincidunt facilisis integer urna, quam faucibus faucibus. Neque, cursus nibh vitae
-          vestibulum, pharetra justo, at venenatis euismod. Massa at adipiscing egestas auctor. Ac massa nisl nibh
-          egestas sed.
-        </Text>
-      </Flex>
+        <Info>
+          {info}
+          {isTruncated && (
+            <Info color="linkColor" onClick={() => setIsExpanded(true)}>
+              read more
+            </Info>
+          )}
+        </Info>
+      </InfoWrapper>
     </Wrapper>
   )
 }
