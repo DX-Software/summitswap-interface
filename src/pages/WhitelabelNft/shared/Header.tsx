286180c1388c5c2f28688eaf506cf4b4125fa989
sxio <stephen.huang96@gmail.com>
2022-09-27 14:16:18 +0700

    refactor(whitelabel nft): details header

--- src/pages/WhitelabelNft/shared/Header.tsx
+++ src/pages/WhitelabelNft/shared/Header.tsx
@@ -17,20 +17,17 @@ const Header = ({ levels }: HeaderProps) => {
     <>
       <Flex borderBottom="1px solid" borderBottomColor="inputColor" paddingBottom="8px" marginBottom="24px">
         <Breadcrumbs>
-          {levels.map((level, index) => {
-            if (index !== levels.length - 1) {
-              return (
-                <Text color="primaryDark" style={{ cursor: 'pointer' }} onClick={level.onBack}>
-                  {level.label}
-                </Text>
-              )
-            }
-            return (
-              <Text color="borderColor" style={{ fontWeight: 700 }}>
+          {levels.map((level, index) =>
+            index !== levels.length - 1 ? (
+              <Text color="primaryDark" style={{ cursor: 'pointer' }} onClick={level.onBack} key={level.label}>
+                {level.label}
+              </Text>
+            ) : (
+              <Text color="borderColor" style={{ fontWeight: 700 }} key={level.label}>
                 {level.label}
               </Text>
             )
-          })}
+          )}
         </Breadcrumbs>
       </Flex>
       <Flex style={{ columnGap: '8px', cursor: 'pointer' }} onClick={levels[secondLastIndex].onBack}>
