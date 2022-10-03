02169c5aa787c5a086c375499a4660b45c35463e
sxio <stephen.huang96@gmail.com>
2022-10-03 12:58:32 +0700

    refactor(whitelabel nft): Header breadcrumb

--- src/pages/WhitelabelNft/shared/Header.tsx
+++ src/pages/WhitelabelNft/shared/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 import { ArrowBackIcon, Breadcrumbs, Flex, Text } from '@koda-finance/summitswap-uikit'
 import React from 'react'
 
@@ -17,17 +18,20 @@ const Header = ({ levels }: HeaderProps) => {
     <>
       <Flex borderBottom="1px solid" borderBottomColor="inputColor" paddingBottom="8px" marginBottom="24px">
         <Breadcrumbs>
-          {levels.map((level, index) =>
-            index !== levels.length - 1 ? (
-              <Text color="primaryDark" style={{ cursor: 'pointer' }} onClick={level.onBack} key={level.label}>
-                {level.label}
-              </Text>
-            ) : (
-              <Text color="borderColor" style={{ fontWeight: 700 }} key={level.label}>
+          {levels.map((level, index) => {
+            const isLast = index === levels.length - 1
+            return (
+              <Text
+                color={isLast ? 'borderColor' : 'primaryDark'}
+                fontWeight={isLast ? 700 : 400}
+                style={{ cursor: isLast ? 'inherit' : 'pointer' }}
+                onClick={isLast ? () => null : level.onBack}
+                key={`${level.label}-${index}`}
+              >
                 {level.label}
               </Text>
             )
-          )}
+          })}
         </Breadcrumbs>
       </Flex>
       <Flex style={{ columnGap: '8px', cursor: 'pointer' }} onClick={levels[secondLastIndex].onBack}>
