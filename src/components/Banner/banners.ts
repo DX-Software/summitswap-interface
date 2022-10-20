811fde39dd1ec70b843b4a4ba65491bc484bded8
erwin-huang <erwinhuank@gmail.com>
2022-10-20 16:46:08 +0700

    feat: remove kapex from claim stacking reward

--- src/components/Banner/banners.ts
+++ src/components/Banner/banners.ts
@@ -1,22 +1,21 @@
-import KodaSmallBanner from '../../img/banners/koda/koda_small_v2.gif'
-import KodaMediumBanner from '../../img/banners/koda/koda_medium_v2.gif'
-import KodaLargeBanner from '../../img/banners/koda/koda_large_v2.gif'
-import EndgameSmallBanner from '../../img/banners/endgame/endgame_small.gif'
-import EndgameMediumBanner from '../../img/banners/endgame/endgame_medium.gif'
 import EndgameLargeBanner from '../../img/banners/endgame/endgame_large.gif'
+import EndgameMediumBanner from '../../img/banners/endgame/endgame_medium.gif'
+import EndgameSmallBanner from '../../img/banners/endgame/endgame_small.gif'
+import KodaLargeBanner from '../../img/banners/koda/koda_large_v2.gif'
+import KodaMediumBanner from '../../img/banners/koda/koda_medium_v2.gif'
+import KodaSmallBanner from '../../img/banners/koda/koda_small_v2.gif'
 
 const BANNERS = {
-    koda: {
-        gifs: [KodaLargeBanner, KodaMediumBanner, KodaSmallBanner],
-        link: "https://koda.finance/",
-        delay: 6200
-    },
-    endgame: {
-        gifs: [EndgameLargeBanner, EndgameMediumBanner, EndgameSmallBanner],
-        link: "https://summitswap.finance/#/swap?output=0x373e4b4E4D328927bc398A9B50e0082C6f91B7bb",
-        delay: 5200
-    },
-
+  koda: {
+    gifs: [KodaLargeBanner, KodaMediumBanner, KodaSmallBanner],
+    link: 'https://koda.finance/',
+    delay: 6200,
+  },
+  endgame: {
+    gifs: [EndgameLargeBanner, EndgameMediumBanner, EndgameSmallBanner],
+    link: 'https://summitswap.finance/#/swap?output=0x373e4b4E4D328927bc398A9B50e0082C6f91B7bb',
+    delay: 5200,
+  },
 }
 
 export default BANNERS
