a3d5f06a71c5a73e68b9ca7265612b2b394fb3cc
sxio <stephen.huang96@gmail.com>
2022-12-05 16:23:08 +0700

    feat(http): remove summitWalletToken when error

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { BACKEND_API } from 'constants/index'
+import { WALLET_LOGIN_ACCESS_TOKEN_KEY } from 'constants/walletLogin'
 
 const httpClient = axios.create({
   baseURL: `${BACKEND_API}`,
@@ -15,6 +16,7 @@ httpClient.interceptors.response.use(
   (error) => {
     // eslint-disable-next-line no-console
     console.log(error)
+    localStorage.removeItem(WALLET_LOGIN_ACCESS_TOKEN_KEY)
     return Promise.reject(error)
   }
 )
