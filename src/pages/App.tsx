175477b20e94600325815922994f168de5cfb1ff
erwin-huang <erwinhuank@gmail.com>
2023-03-21 07:37:09 +0700

    feat: remove all menus except staking

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -162,8 +162,8 @@ export default function App() {
             <Popups />
             <Web3ReactManager>
               <Switch>
-                <Route exact strict path={['/', '/send']}>
-                  <Redirect to={`/swap${location.search}`} />
+                <Route exact strict path={['/']}>
+                  <Redirect to='/staking/deposit' />
                 </Route>
                 <Route exact path="/whitelabel-nft/:nftAddress" component={MintWidget} />
                 <Menu>
